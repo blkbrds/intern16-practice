1b37dd498bc988680fe187a5c240e0084e28ce70
minh.nguyen T[4] <minh.nguyen4@asiantech.vn>
2020-06-24 08:41:14 +0700

    Thêm mô tả Review lại file

--- 01_GioiThieu/MinhNguyen/baitap1/baitap1/HomeViewController.swift
+++ 01_GioiThieu/MinhNguyen/baitap1/baitap1/HomeViewController.swift
@@ -11,15 +11,19 @@ import UIKit
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var btnTap: UIButton!
+    
+    // Thay đổi màu background khi click button
     @IBAction func changeColor(_ sender: Any) {
         view.backgroundColor = color()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        //màu background cho button
         btnTap.backgroundColor = .green
 
         // Do any additional setup after loading the view.
     }
+    // Trả về random màu
     func color()->UIColor{
         return UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
     }
