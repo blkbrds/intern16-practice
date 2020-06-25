2e80b48ec011a9c2f0672eb3909977ebd3743200
PCI0001 <tam.nguyen7@asiantech.vn>
2020-06-25 13:48:09 +0700

    no message

--- 02_SwiftBasic/TAMNGUYEN/Baiso5.playground/Contents.swift
+++ 02_SwiftBasic/TAMNGUYEN/Baiso5.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 func Find(){
-    let str1 = "em yeu anh em yeu anh"
+    let str1 = "em yeu anh em yeu anh em yeu anh nhieu lam ghet anh yeu anh"
     let str2 = "anh"
     let a = str1.components(separatedBy: str2)
     print("So lan xuat hien:\(a.count - 1)")
