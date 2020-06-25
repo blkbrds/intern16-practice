25590b60256d630c7245a968f267c4f4b3c8f0bb
HoaNguyen <hoa.nguyen2@asiantech.vn>
2020-06-25 14:49:54 +0700

    Finish excise 2 part 5,6,7

---
+++ 02_SwiftBasic/HOANGUYEN/baitap02/baitap2.06.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+//shuffle 1 mảng
+
+func shuffleArr(_ arr: inout Array<Int>)  -> Array<Int> {
+    let n:Int = arr.count
+    var i:Int = 0
+    var j:Int = 0
+    while(i < n){
+        j = Int.random(in: 0 ..< n)
+        let temp = arr[j]
+        arr[j] = arr[i]
+        arr[i] = temp
+        i += 1
+    }
+    return arr
+}
+var a:Array<Int> = [1,2,3,4,5,6,7,8,9]
+shuffleArr(&a)
+
+
