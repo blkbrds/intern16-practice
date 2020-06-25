25e2df5f0c11aea5cc341d1daa01df3a621b1a89
AT_HaVan <ha.van@asiantech.vn>
2020-06-25 17:00:41 +0700

    Xong bài 7

---
+++ 02_SwiftBasic/HaVan/BaiTap7.playground/Contents.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+
+func bearrayReplace(inputArray array: [Int], elemToReplace replace: Int,
+                    substitutionElem sub: Int) -> [Int] {
+    var newArr = array
+    for i in 0...newArr.count - 1 {
+        if newArr[i] == replace {
+            newArr[i] = sub
+        }
+    }
+    return newArr
+}
+print("Thay thế 3 vào mảng [1,2,1], ta được kết quả: \(bearrayReplace(inputArray: [1,2,1], elemToReplace: 1, substitutionElem: 3))")
