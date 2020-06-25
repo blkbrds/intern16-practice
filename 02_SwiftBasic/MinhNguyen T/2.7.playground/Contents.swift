a264c6ffccff75b2341e20bdeb7cfaa814121a7b
minh.nguyen T[4] <minh.nguyen4@asiantech.vn>
2020-06-25 16:14:20 +0700

    xong bài 2.7

---
+++ 02_SwiftBasic/MinhNguyen T/2.7.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+
+// Thay đổi gia trị cũ trong mãng thành giá trị mơi
+
+func changeMang(mang:[Int],giaTriCu:Int,giaTriMoi:Int)->[Int]{
+    var bienTam = mang
+    for i in 0..<bienTam.count where bienTam[i] == giaTriCu {
+        bienTam[i] = giaTriMoi
+    }
+    return bienTam
+}
+
+changeMang(mang: [1,3,1], giaTriCu: 1, giaTriMoi: 2)
+
+
