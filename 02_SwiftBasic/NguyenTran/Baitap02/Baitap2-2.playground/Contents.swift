4b0d21fff42592c48013dda5ea637a776cf81695
Nguyen Tran T <nguyen.tran@asiantech.vn>
2020-06-26 09:15:43 +0700

    Sua bai tap 2-2, 2-3 theo kieu enum & func

--- 02_SwiftBasic/NguyenTran/Baitap02/Baitap2-2.playground/Contents.swift
+++ 02_SwiftBasic/NguyenTran/Baitap02/Baitap2-2.playground/Contents.swift
@@ -1,37 +1,53 @@
 import UIKit
 import Foundation
 
-let a:Float = 1, b:Float = 4, c:Float = 3
+enum nghiem {
+    case vosonghiem
+    case vonghiem
+    case motnghiem(Float)
+    case hainghiem(Float,Float)
+}
 
-print("Giai phuong trinh bac 2:   \(a)x^2 + \(b)x + c = 0")
-
-var x1:Float = 0, x2:Float = 0
-if a==0 {
-    if b==0 {
-        if c==0 {
-            print("Phuong trinh co nghiem voi moi x.")
+func ptbac2(a:Float, b:Float, c:Float) -> nghiem {
+    if a == 0 {
+        if b == 0 {
+            if c == 0 {
+                return .vosonghiem
+            }
+            else {
+                return .vonghiem
+            }
         }
         else {
-            print("Phuong trinh vo nghiem.")
+            return .motnghiem((-c)/b)
         }
     }
     else {
-        x1 = Float((-c)/b)
+        let delta:Float = b*b-4*a*c
+        if delta < 0 {
+            return .vonghiem
+        }
+        else if delta == 0 {
+            return .motnghiem((-b)/(2*a))
+        }
+        else {
+            let sq: Float = sqrtf(delta)
+            let x1:Float = (-b+sq)/(2*a)
+            let x2:Float = (-b-sq)/(2*a)
+            return .hainghiem(x1, x2)
+        }
     }
 }
-else {
-    let delta:Float = b*b-4*a*c
-    if delta<0 {
-        print("Phuong trinh vo nghiem.")
-    }
-    else if delta==0 {
-        x1 = Float((-b)/(2*a))
-        print("Phuong trinh co nghiem kep x = \(x1)")
-    }
-    else {
-        let sq: Float = sqrtf(delta)
-        x1 = (-b+sq)/(2*a)
-        x2 = (-b-sq)/(2*a)
-        print("Phuong trinh co hai nghiem x1 = \(x1) va x2 = \(x2)")
-    }
+
+let a:Float = 1, b:Float = 4, c:Float = 3
+
+switch ptbac2(a:a, b:b, c:c) {
+case .vosonghiem:
+    print("Phuong trinh co nghiem voi moi x.")
+case .vonghiem:
+    print("Phuong trinh vo nghiem.")
+case .motnghiem(let x):
+    print("Phuong trinh co 1 nghiem la x=\(x).")
+case .hainghiem(let x1, let x2):
+    print("Phuong trinh co 2 nghiem la x1=\(x1) va x2=\(x2).")
 }
