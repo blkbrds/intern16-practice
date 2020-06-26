be801044c843aeea5df72c709bcf4a7721a7720b
hieu.ngq <hieu.nguyen3@asiantech.vn>
2020-06-26 16:49:43 +0700

    Xong 3.6 -> 3.7

--- 03_SwiftOOP/HieuNguyen/BaiTap3.6.playground/Contents.swift
+++ 03_SwiftOOP/HieuNguyen/BaiTap3.6.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 import Foundation
 var pi = Float.pi
 class HinhVe {
-
+    
 }
 
 class HaiChieu: HinhVe {
@@ -71,9 +71,29 @@ class Cau: BaChieu {
         banKinh = bk
     }
     
-//    func
+    // dien tich hinh cau: S = 4 x π x r2 = π x d2
+    func dienTich() -> Float {
+        return 4 * pi * banKinh * banKinh
+    }
+    // the tich hinh cau: 4/3 x π x r3
+    func theTich() -> Float {
+        return 4 / 3 * pi * pow(banKinh, 3)
+    }
+    
 }
 
 class LapPhuong: BaChieu {
-
+    var canh: Float
+    
+    init(canh: Float) {
+        self.canh = canh
+    }
+    
+    func theTich() -> Float {
+        return pow(canh, 3)
+    }
+    
+    func dienTich() -> Float {
+        return 6 * pow(canh, 2)
+    }
 }
---
+++ 03_SwiftOOP/HieuNguyen/BaiTap3.7.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+class hocSinh {
+    var hoTen: String
+    var namSinh: String
+    var tongDiem: Float
+
+    init(hoten: String,namsinh: String,tongdiem: Float) {
+        self.hoTen = hoten
+        self.namSinh = namsinh
+        self.tongDiem = tongdiem
+    }
+    
+}
+
+func sapxep(mangHocSinh: [hocSinh]) -> [hocSinh]{
+    let mangHocSinh: [hocSinh] = mangHocSinh
+    var result: [hocSinh] = mangHocSinh.sorted { (hs0: hocSinh , hs1: hocSinh) -> Bool in
+        if hs0.tongDiem > hs1.tongDiem {
+            return true
+        }
+        if hs0.tongDiem == hs1.tongDiem {
+            return hs0.namSinh > hs1.namSinh
+        }
+        return false
+    }
+    return result
+}
+
+extension StringProtocol {
+    var firstUppercased: String { return prefix(1).uppercased() + dropFirst() }
+}
+
+var hs1 = hocSinh(hoten: "Nguyen Quang hieu", namsinh: "1994", tongdiem: 5)
+var hs2 = hocSinh(hoten: "Phan Ngoc khanh", namsinh: "1997", tongdiem: 7)
+var hs3 = hocSinh(hoten: "Tran T K Anh", namsinh: "1995", tongdiem: 10)
+var hs4 = hocSinh(hoten: "Tran T K Oanh", namsinh: "1996", tongdiem: 7)
+
+var dsHs = sapxep(mangHocSinh: [hs1,hs4,hs3,hs2])
+
+for hs in dsHs {
+    print(hs.hoTen.firstUppercased)
+}
