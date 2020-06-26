56050fa04ca34cbfe1ed26a4e2ee9ad58a3e1cd6
Nguyen Tran T <nguyen.tran@asiantech.vn>
2020-06-26 14:24:40 +0700

    Sua bai hoan vi theo kieu viet func

--- 02_SwiftBasic/NguyenTran/Baitap02/Baitap2-1.playground/Contents.swift
+++ 02_SwiftBasic/NguyenTran/Baitap02/Baitap2-1.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 
 func circle(r: Float) -> (Float, Float) {
     let pi: Float = 3.14159
-    let V: Float = Float(3 / 4 * r * r * r * pi)
+    let V: Float = Float(4 / 3 * r * r * r * pi)
     let S: Float = Float(4 * r * r * pi)
     return (V, S)
 }
