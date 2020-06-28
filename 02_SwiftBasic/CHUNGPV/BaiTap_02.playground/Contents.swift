f6b1cd940101928906301790a4d8ba0d5bb97ccf
Chung <chung.pham@asiantech.vn>
2020-06-28 16:23:43 +0900

    Fix bai 2, bai 3 Phan 2. Basic Swift :  Xoa print o trong ham thay bang Enum

--- 02_SwiftBasic/CHUNGPV/BaiTap_02.playground/Contents.swift
+++ 02_SwiftBasic/CHUNGPV/BaiTap_02.playground/Contents.swift
@@ -1,24 +1,44 @@
 //import UIKit
 import Foundation
 
-public func phuongtrinhbac2(a: Float, b: Float, c: Float) -> (x1: Float, x2: Float) {
+enum KetQua{
+    case vonghiem
+    case hainghiem(Float, Float)
+    case motnghiem(Float)
+    case nghiemkep(Float)
+}
+
+func phuongtrinhbac2(a: Float, b: Float, c: Float) -> KetQua {
+    if a == 0 {
+        if b == 0 {
+            return .vonghiem
+        } else {
+            return .motnghiem(-c / b)
+        }
+    }
     var x1 : Float
     var x2 : Float
     let delta : Float = b * b - 4 * a * c
-    if delta < 0 {
-        x1 = 0.0
-        x2 = 0.0
-        return (x1,x2)
-    } else if delta == 0 {
-            x1 = -b/(2*a)
-            x2 = x1
-            return (x1,x2)
-    } else {
+    if delta > 0 {
         x1 = (-b + delta) / (2*a);
         x2 = (-b - delta) / (2*a);
-        return (x1,x2)
+        return .hainghiem(x1, x2)
+    } else if delta == 0 {
+        return .nghiemkep(-b / (2 * a))
+    } else {
+        return .vonghiem
     }
-        
 }
 
-phuongtrinhbac2(a: 3, b: 4, c: 1)
+let kq = phuongtrinhbac2(a: 3, b: 4, c: 1)
+
+switch kq {
+case .vonghiem:
+    print("Phuong trinh vo nghiem")
+case .motnghiem(let x1):
+    print("Phuong trinh 1 nghiem x: = \(x1)")
+case .hainghiem(let x1, let x2):
+    print("Phuong trinh co 2 nghiem x1= \(x1), x2 = \(x2)")
+case .nghiemkep(let x):
+    print("Phuong trinh co nghien kep x1 = x2 = \(x)")
+}
