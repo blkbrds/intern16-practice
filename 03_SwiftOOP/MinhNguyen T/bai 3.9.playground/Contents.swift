c0050d54f83d85c18f0ad2448d1bc4122a1adc2d
minh.nguyen T[4] <minh.nguyen4@asiantech.vn>
2020-06-29 09:52:28 +0700

    Bai 3.9 OOP

---
+++ 03_SwiftOOP/MinhNguyen T/bai 3.9.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+
+class mang1C{
+    
+    var arr:[Int] = []
+    
+    init(arr: [Int]) {
+        self.arr = arr
+    }
+    
+    func inMang(){
+        print ("\(arr)")
+    }
+    
+    func maxArr()-> Int{
+        
+        var max = arr[0]
+        for i in 1...arr.count - 1{
+            if arr[i] > max{
+                max = arr[i]
+            }
+        }
+        return max
+    }
+    func minArr()-> Int{
+           
+           var min = arr[0]
+           for i in 1...arr.count - 1{
+               if arr[i] < min{
+                   min = arr[i]
+               }
+           }
+           return min
+       }
+}
+
+var arr1 = mang1C(arr: [4,6,8,2,6,3])
+arr1.inMang()
+arr1.minArr()
+arr1.maxArr()
