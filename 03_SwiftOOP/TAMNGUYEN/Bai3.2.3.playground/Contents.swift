24f83cbc69ae2b2f4a6ccd27342ee46ae6383bd4
PCI0001 <tam.nguyen7@asiantech.vn>
2020-06-29 08:03:58 +0700

    Hoan thành bài tập 3.1 + 3.2 + 3.3 + Bài tập phân số + Bài tập đa giác , tam giác

---
+++ 03_SwiftOOP/TAMNGUYEN/Bai3.2.3.playground/Contents.swift
@@ -0,0 +1,54 @@
+import UIKit
+import Foundation
+
+class DaGiac {
+    var soCanh: Int
+    var sizeCanh: [Float]
+    
+    
+    init(soCanh: Int, sizeCanh: [Float]) {
+        self.soCanh = soCanh
+        self.sizeCanh = sizeCanh
+    }
+    
+    func tinhChuVi() -> Float {
+        var chuVi: Float = 0
+        for index in 0..<sizeCanh.count {
+            chuVi += sizeCanh[index]
+        }
+        return chuVi
+    }
+    
+    func printSize() {
+        for index in 0..<sizeCanh.count {
+            print(sizeCanh[index])
+        }
+    }
+    
+}
+var  dagiac = DaGiac(soCanh: 5, sizeCanh: [1,2,3,4,5])
+dagiac.tinhChuVi()
+class TamGiac: DaGiac {
+    override func tinhChuVi() -> Float {
+        var chuVi: Float = 0
+        for index in 0..<sizeCanh.count {
+            chuVi += sizeCanh[index]
+        }
+        return chuVi
+        
+    }
+    
+    func dienTich() -> Float {
+        let nuaChuVi = self.tinhChuVi() / 2
+        var a = nuaChuVi - self.sizeCanh[0]
+        var b = nuaChuVi - self.sizeCanh[1]
+        var c = nuaChuVi - self.sizeCanh[2]
+        return sqrt(nuaChuVi * a * b * c )
+    }
+}
+var tamgiac = TamGiac(soCanh: 3,sizeCanh: [2,1,3] )
+tamgiac.dienTich()
+tamgiac.tinhChuVi()
+print("Dien tich cua tam giac la : \(tamgiac.dienTich())")
+
+
