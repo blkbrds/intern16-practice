f37149a1dcc1794913d2050009e0324fa2fcac8b
PCI0001 <tam.nguyen7@asiantech.vn>
2020-06-29 16:29:06 +0700

    Sua bai 2.3

--- 02_SwiftBasic/TAMNGUYEN/Baiso3.playground/Contents.swift
+++ 02_SwiftBasic/TAMNGUYEN/Baiso3.playground/Contents.swift
@@ -1,27 +1,31 @@
 import UIKit
-// giai he phuong trinh bac nhat 2 an
-var x: Float
-var y: Float
-func d(a1: Float,a2: Float,b1: Float,b2: Float) -> Float{
+
+var x: Float = 0.0
+var y: Float = 0.0
+
+func d(a1: Float,a2: Float,b1: Float,b2: Float) -> Float {
     return Float(a1 * b2 - a2 * b1)
 }
-func dX(b1: Float,b2: Float,c1: Float,c2: Float) -> Float{
+
+func dX(b1: Float,b2: Float,c1: Float,c2: Float) -> Float {
     return Float(c1 * b2 - c2 * b1)
 }
-func dY(a1: Float,a2: Float,c1: Float,c2: Float) -> Float{
+
+func dY(a1: Float,a2: Float,c1: Float,c2: Float) -> Float {
     return Float(a1 * c2 - a2 * c1)
 }
-let kqD = d(a1: 1, a2: 2, b1: 3, b2: 4)
-let kqdX = dX(b1: 3, b2: 4, c1: -1, c2: -2)
-let kqdY = dY(a1: 1, a2: 2, c1: -1, c2: -2)
-if (kqD == 0 ){
-    if(kqdX + kqdY == 0)
-    {print("he phuong trinh co vo so nghiem")}
-    else{
+
+let kqD: Float = d(a1: 1, a2: 2, b1: 3, b2: 4)
+let kqdX: Float = dX(b1: 3, b2: 4, c1: -1, c2: -2)
+let kqdY: Float = dY(a1: 1, a2: 2, c1: -1, c2: -2)
+
+if kqD == 0 {
+    if kqdX + kqdY == 0 {
+        print("he phuong trinh co vo so nghiem")
+    } else {
         print("he phuong trinh vo nghiem")
     }
-}
-else{
+} else {
     x = kqdX / kqD
     y = kqdY / kqD
     print("he phuong trinh co nghiem (x,y) = \(x),\(y)")
