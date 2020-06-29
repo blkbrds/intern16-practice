3502af00b9fe6740500ba51c640fd566c06de5d7
PCI0001 <tam.nguyen7@asiantech.vn>
2020-06-29 10:09:27 +0700

    Hoan thanh bai tap 3.6 + 3.7

---
+++ 03_SwiftOOP/TAMNGUYEN/Bai3.6.playground/Contents.swift
@@ -0,0 +1,64 @@
+import UIKit
+import Foundation
+var pi = Float.pi
+class HinhVe {
+    
+}
+class HaiChieu:HinhVe {
+    
+}
+class BaChieu:HinhVe {
+    
+}
+class Tron:HaiChieu {
+    var bankinh: Float
+    init(r: Float) {
+        bankinh = r
+    }
+    
+    func dienTich() -> Float {
+        return pi * bankinh * bankinh
+    }
+    
+    func chuVi() -> Float {
+        return 2 * pi * bankinh
+    }
+}
+class Vuong:HaiChieu {
+    var canh: Float
+    init(c: Float) {
+        canh = c
+    }
+    
+    func dienTich() -> Float {
+        return canh * canh
+    }
+    
+    func chuVi() -> Float {
+        return 4 * canh
+    }
+}
+class TamGiac:HaiChieu {
+    var a,b,c: Float
+    init(a: Float, b: Float, c: Float) {
+        self.a = a
+        self.b = b
+        self.c = c
+    }
+    
+    func dienTich() -> Float {
+        var nuachuvi = chuVi() / 2
+        return 1
+        // return sqrt(nuachuvi * (nuachuvi - a) * (nuachuvi - b) * (nuachuvi - c ))
+    }
+    
+    func chuVi() -> Float {
+        return a + b + c
+    }
+}
+class Cau:BaChieu {
+    // something
+}
+class LapPhuong:BaChieu {
+// something
+}
---
+++ 03_SwiftOOP/TAMNGUYEN/Bai3.7.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+class HocSinh {
+    var hoTen: String
+    var namSinh: Int
+    var tongDiem: Float
+    init(hoTen: String, namSinh: Int, tongDiem: Float) {
+        self.hoTen = hoTen
+        self.namSinh = namSinh
+        self.tongDiem = tongDiem
+    }
+}
+    func sapXep(manghs: [HocSinh]) -> [HocSinh] {
+        let manghs: [HocSinh] = manghs
+        var ketqua: [HocSinh] = manghs.sorted { (hs0: HocSinh, hs1: HocSinh) -> Bool in
+            if hs0.tongDiem > hs1.tongDiem {
+                return true
+            }
+            if hs0.namSinh == hs1.namSinh {
+                hs0.namSinh > hs1.namSinh
+            }
+            return false
+        }
+        return ketqua
+    }
+    
+
+var hs1 = HocSinh(hoTen: "Tam", namSinh: 97, tongDiem: 10)
+var hs2 = HocSinh(hoTen: "An", namSinh: 96, tongDiem: 7)
+var hs3 = HocSinh(hoTen: "Bao", namSinh: 93, tongDiem: 8)
+var hs4 = HocSinh(hoTen: "Ha", namSinh: 80, tongDiem: 1)
+var hs5 = HocSinh(hoTen: "Yen", namSinh: 96, tongDiem: 7)
+let danhsach = sapXep(manghs: [hs1, hs2, hs3, hs4, hs5])
