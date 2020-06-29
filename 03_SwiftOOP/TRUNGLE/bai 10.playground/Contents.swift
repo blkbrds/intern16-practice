0fe058771bc3abc3a86e30c56b61952bcbd82412
at-trungle <trung.le@asiantech.vn>
2020-06-29 22:01:12 +0700

    TrungLeD - Bai tap 10 - Tính tổng dãy số 1 đến n

---
+++ 03_SwiftOOP/TRUNGLE/bai 10.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str = "Hello, playground"
+class A {
+     var a: Int
+    init(a: Int) {
+        self.a = a
+    }
+    func sumA() -> Int {
+        var sum = 0
+        for i in 1...a {
+            sum += i
+        }
+        return sum
+    }
+}
+
+var a = A(a: 15)
+print("Tong S tu 1 den a la: \(a.sumA())")
