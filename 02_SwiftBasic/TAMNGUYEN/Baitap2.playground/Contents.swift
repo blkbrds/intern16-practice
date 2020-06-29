a5d09213a9d78cd4fc5ab2b6df9a0c8b3b1f6a0f
PCI0001 <tam.nguyen7@asiantech.vn>
2020-06-29 16:55:37 +0700

    Sua bai 2

--- 02_SwiftBasic/TAMNGUYEN/Baitap2.playground/Contents.swift
+++ 02_SwiftBasic/TAMNGUYEN/Baitap2.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 // giai phuong trinh bac 2
+
 func denta(a: Float, b: Float, c: Float) -> Float {
     return Float(b * b - 4 * a * c)
 }
@@ -7,19 +8,21 @@ func denta(a: Float, b: Float, c: Float) -> Float {
 func tinhNghiem(a : Float,b : Float,c : Float) -> (n1: Float, n2: Float) {
     return (Float(-b - sqrt(a) / ( 2 * a)), Float(-b + sqrt(a) / ( 2 * a)))
 }
+
 func tinhnghiemKep(a: Float,b : Float,c : Float) -> Float {
     return Float(-b / (2 * a))
 }
-let del = denta(a: 2,b: 4,c: -6)
+
+let del: Float = denta(a: 2, b: 4, c: -6)
+
 if del < 0 {
     print("Phuong trinh vo nghiem")
 }
+
 if del == 0 {
-    print("Phuong trinh co nghiem kep: \(tinhnghiemKep(a: 1,b: 2,c: -3))")
+    print("Phuong trinh co nghiem kep: \(tinhnghiemKep(a: 1, b: 2, c: -3))")
 }
+
 if del > 0 {
-    print("Phuong trinh co 2 nghiem : \(tinhNghiem(a: 1,b: 2,c: -3))")}
-	
-
-
-
+    print("Phuong trinh co 2 nghiem : \(tinhNghiem(a: 1, b: 2, c: -3))")
+}
