fd60805f6ae375172b3f96daef7f0a326c8b783f
Hieu.ngq <67259480+at-hieunguyen3@users.noreply.github.com>
2020-06-29 09:22:35 +0700

    Fix func pow

--- 02_SwiftBasic/HieuNguyen/BaiTap2.1.playground/Contents.swift
+++ 02_SwiftBasic/HieuNguyen/BaiTap2.1.playground/Contents.swift
@@ -2,8 +2,8 @@ import UIKit
 import Foundation
 func hinhcau (bankinh : Float) -> (Float,Float){
     let pi = Float.pi
-    let dientich:Float = 4 * pi * bankinh * bankinh
-    let thetich:Float = ( 4 / 3 ) * pi * bankinh * bankinh * bankinh
+    let dientich:Float = 4 * pi * pow(bankinh, 2)
+    let thetich:Float = ( 4 / 3 ) * pi * pow(bankinh, 3)
     
     return(dientich,thetich)
 }
