d9cc32d5bb1cde07be6aa09d2194bd4f77813d5e
AT_HaVan <ha.van@asiantech.vn>
2020-06-30 08:08:48 +0700

    Chỉnh sửa xong bài tập 1

--- 02_SwiftBasic/HaVan/BaiTap1.playground/Contents.swift
+++ 02_SwiftBasic/HaVan/BaiTap1.playground/Contents.swift
@@ -1,15 +1,13 @@
 import UIKit
 import Foundation
-let pi:Float = 3.14
-func calculator(with radius: Double) -> (Double, Double) {
+let pi:Double = 3.14
+func calculator(with radius: Double) -> (s: Double, v: Double) {
     let s = 4 * Double(pi) * pow(radius, 2)
     let v = (4 / 3) * ( Double(pi) * pow(radius, 3))
-    
     return (s,v)
-
 }
 
-let resultSByString = String(format: "%.2f", calculator(with: 2).0)
-let resultVByString = String(format: "%.2f", calculator(with: 2).1)
+let resultSByString = String(format: "%.2f", calculator(with: 2).s)
+let resultVByString = String(format: "%.2f", calculator(with: 2).v)
 print("Diện tích hình cầu với đường kính 2 là : \(resultSByString)")
 print("Thể tích hình cầu với đường kính 2 là : \(resultVByString)")
