1787a47c80799617bcb23dbd951269cd647bc62f
PCI0004 <trin.nguyen@asiantech.vn>
2020-06-30 13:42:26 +0700

    Update bai tap swift basic theo comment

--- 02_SwiftBasic/TrinNguyen/BaiTap03.playground/Contents.swift
+++ 02_SwiftBasic/TrinNguyen/BaiTap03.playground/Contents.swift
@@ -1,9 +1,9 @@
 import UIKit
 
 func GPTB1 (a1: Float, b1: Float, c1: Float, a2: Float, b2: Float, c2: Float) {
-    let D: Float = a1 * b2 - a2 * b1;
-    let Dx: Float = c1 * b2 - c2 * b1;
-    let Dy: Float = a1 * c2 - a2 * c1;
+    let D: Float = a1 * b2 - a2 * b1
+    let Dx: Float = c1 * b2 - c2 * b1
+    let Dy: Float = a1 * c2 - a2 * c1
     if(D == 0) {
         if(Dx + Dy == 0) {
             print("He phuong trinh co vo so nghiem") }
