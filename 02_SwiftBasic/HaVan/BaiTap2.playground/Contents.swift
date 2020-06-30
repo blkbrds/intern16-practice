896fd297225536b9d43199318ad20be9452fd241
AT_HaVan <ha.van@asiantech.vn>
2020-06-30 09:04:58 +0700

    Sửa xong bài tập 2,3 - Áp dụng enum vào kết quả

--- 02_SwiftBasic/HaVan/BaiTap2.playground/Contents.swift
+++ 02_SwiftBasic/HaVan/BaiTap2.playground/Contents.swift
@@ -1,58 +1,65 @@
 import Foundation
-func ptbac1(num1 a: Int, num2 b: Int) -> Any {
+
+enum NghiemPhuongTrinh {
+    case PTVoNghiem
+    case PTVoSoNghiem
+    case PTCo1Nghiem(x: Double)
+    case PTCo2Nghiem(x1: Double, x2: Double)
+}
+
+func ptbac1(num1 a: Int, num2 b: Int) -> NghiemPhuongTrinh {
     if a == 0 {
         if b != 0 {
-            return "vô nghiệm"
+            return .PTVoNghiem
         }
         else {
-        return "vô số nghiệm"
-           
+            return .PTVoSoNghiem
         }
     }
-    else if b == 0{
-        return 0
+    else if b == 0 {
+        return .PTCo1Nghiem(x: 0)
     }
     else {
-        return Double(-b/a)
+        return .PTCo1Nghiem(x: Double(-b/a))
     }
-    
 }
-func ptbac2(num1 a: Int, num2 b: Int, num3 c: Int) -> (Any,Double?) {
+
+func ptbac2(num1 a: Int, num2 b: Int, num3 c: Int) -> NghiemPhuongTrinh {
     
     if a == 0 {
-        return (ptbac1(num1: b, num2: c), nil)
+        return ptbac1(num1: b, num2: c)
     } else if b == 0 && c == 0 {
-        return (0,nil)
+        return .PTCo1Nghiem(x: 0)
         
     } else if b == 0 && c != 0 {
         let x = -c/a
-        return ((sqrt(Double(x))),-(sqrt(Double(x))))
+        return .PTCo2Nghiem(x1: (sqrt(Double(x))), x2: -(sqrt(Double(x))))
     }
     else {
         let delta = pow(Double(b), 2) - 4*Double(a*c)
       switch delta {
       case ...0:
-        return ("vô nghiệm",nil)
-        // phương trình vô nghiệm
+        return .PTVoNghiem
       case 0 :
-        return (-Double(b)/Double((2*a)),-Double(b)/Double((2*a)))
+        return .PTCo2Nghiem(x1: -Double(b)/Double(2 * a), x2: -Double(b)/Double(2 * a))
       default:
-        let x1 = (  -Double(b) + sqrt(delta)) / Double(2*a)
-        let x2 = (-Double(b) - sqrt(delta)) / Double(2*a)
-       return (x1,x2)
+        return .PTCo2Nghiem(x1: -Double(b) + sqrt(delta) / Double(2*a), x2: -Double(b) - sqrt(delta) / Double(2*a))
       }
-    
     }
 }
 
 let kq = ptbac2(num1: 0, num2: 0, num3: 0)
-let x1 = kq.0
-if let x2 = kq.1 {
-    print("nghiệm của phương trình là: \(x1), \(x2)")
+switch kq {
+case .PTVoNghiem:
+    print("Phương trình vô nghiệm")
+case .PTVoSoNghiem:
+    print("Phương trình vô số nghiệm")
+case .PTCo1Nghiem(x: let x):
+    print("Phương trình có 1 nghiệm x= \(x)")
+case .PTCo2Nghiem(x1: let x1, x2: let x2):
+    print("Phương trình có 2 nghiệm phân biệt: x1 = \(x1) , x2 = \(x2)")
 }
-else {
-    print("nghiệm của phương trình là: \(x1)")
-}
+
       
        
     
--- 02_SwiftBasic/HaVan/BaiTap3.playground/Contents.swift
+++ 02_SwiftBasic/HaVan/BaiTap3.playground/Contents.swift
@@ -1,28 +1,36 @@
 import Foundation
-func giaihe(a1 : Int, b1: Int, c1: Int, a2:Int, b2:Int, c2:Int) -> (Any, Double?){
+
+enum NghiemHe {
+    case HeVoNghiem
+    case HeVoSoNghiem
+    case HeCoNghiem(x: Double, y: Double)
+}
+
+func giaihe(a1 : Int, b1: Int, c1: Int, a2:Int, b2:Int, c2:Int) -> NghiemHe {
     let d = a1*b2 - a2*b1
     let dx = c1*b2 - c2*b1
     let dy = c1*a2 - a2*c1
     if d == 0 {
      if dx == 0 && dy == 0{
-        return ("vô số", nil)
+        return .HeVoSoNghiem
         }
     else {
-        return ("vô nghiệm", nil)
+        return .HeVoNghiem
         }
     }
     else {
-        return (Double(dx/d), Double(dy/d))
+        return .HeCoNghiem(x: Double(dx/d), y: Double(dy/d))
     }
-
 }
-let kq = giaihe(a1: 0, b1: 0, c1: 0, a2: 0, b2: 0, c2: 0)
-var x1 = kq.0
 
-    if let x2 = kq.1{
-        print("Nghiệm của phương trình là x1= \(x1) ; x2= \(x2)" )
-    }
-    else {
-        print("Nghiệm của phương trình là : \(x1)")
-    }
+//MARK: kiểm tra kết quả
+let ketQua = giaihe(a1: 0, b1: 0, c1: 0, a2: 0, b2: 0, c2: 0)
+switch ketQua {
+case .HeVoNghiem:
+    print("Hệ vô nghiệm")
+case .HeVoSoNghiem:
+    print("Hệ vô số nghiệm")
+case .HeCoNghiem(x: let x, y: let y):
+    print("Hệ có nghiệm : x = \(x) và y = \(y)")
+}
 
