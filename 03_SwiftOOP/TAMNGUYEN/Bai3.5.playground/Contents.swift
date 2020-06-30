4bf676c4ed869b85713f2a9070fedc4bb90070da
PCI0001 <tam.nguyen7@asiantech.vn>
2020-06-30 07:50:41 +0700

    Xong bai tap 5

---
+++ 03_SwiftOOP/TAMNGUYEN/Bai3.5.playground/Contents.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+class CStack {
+    var capa: Int = 0
+    var array: [Int] = []
+    init(capa: Int, array: [Int]) {
+        self.capa = capa
+        self.array = array
+    }
+    
+    func checkEmpty() -> Bool {
+        if array.count == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func checkFull() -> Bool {
+        if array.count == capa {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func add(them: Int) -> [Int] {
+        if checkFull() == true {
+            print("Full")
+        }
+        if checkFull() == false {
+            array.append(them)
+        }
+        return array
+    }
+}
+
+let kq = CStack(capa: 4,array: [1,2,3])
+kq.checkFull()
+kq.add(them: 2)
+ 
