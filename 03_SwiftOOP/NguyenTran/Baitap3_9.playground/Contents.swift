5a275c0f0cdd3f61105144e8e9572e8636d608dc
Nguyen Tran T <nguyen.tran@asiantech.vn>
2020-06-30 22:55:06 +0700

    Xong bài tập 9 phần OOP

---
+++ 03_SwiftOOP/NguyenTran/Baitap3_9.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+import Foundation
+
+class mang1c {
+    var arr: Array<Int>
+
+    init(arr: Array<Int>) {
+        self.arr = arr
+    }
+
+    func printArr() {
+        print(arr)
+    }
+
+    func minmax() -> (Int, Int) {
+        if arr == [] {
+            print("Error: Mảng rỗng.")
+            return (0, 0)
+        } else {
+            var max = arr[0]
+            var min = arr[0]
+            for i in 0..<arr.count {
+                if arr[i] > max {
+                    max = arr[i]
+                }
+                if arr[i] < min {
+                    min = arr[i]
+                }
+            }
+            return (min, max)
+        }
+    }
+}
+
+let a = mang1c(arr: [1,4,6,2,29,45])
+
+a.printArr()
+print("Số lớn nhất trong mảng là \(a.minmax().1)\nSố nhỏ nhất trong mảng là \(a.minmax().0)")
