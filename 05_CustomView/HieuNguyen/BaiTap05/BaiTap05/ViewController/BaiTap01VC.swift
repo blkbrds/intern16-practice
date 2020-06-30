4572fa9664a3ff95a6000865d1b5b51982e66279
hieu.ngq <hieu.nguyen3@asiantech.vn>
2020-06-30 14:50:14 +0700

    -Create project without storyboard

---
+++ 05_CustomView/HieuNguyen/BaiTap05/BaiTap05/ViewController/BaiTap01VC.swift
@@ -0,0 +1,30 @@
+//
+//  BaiTap01VC.swift
+//  BaiTap05
+//
+//  Created by PCI0020 on 6/30/20.
+//  Copyright © 2020 hieu.ngq. All rights reserved.
+//
+
+import UIKit
+
+class BaiTap01VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
