8d09ffa9bf5bb67a83b0411a7a312cadd96145e5
hieu.ngq <hieu.nguyen3@asiantech.vn>
2020-07-01 09:41:16 +0700

    Fix bai slider custom

--- 04_View/HieuNguyen/BaiTap04/BaiTap04/Controller/BaiTap05VC.swift
+++ 04_View/HieuNguyen/BaiTap04/BaiTap04/Controller/BaiTap05VC.swift
@@ -1,33 +1,60 @@
+//
+//  BaiTap05VC.swift
+//  BaiTap04
+//
+//  Created by PCI0020 on 7/1/20.
+//  Copyright © 2020 hieungq. All rights reserved.
+//
+
 import UIKit
 
 class BaiTap05VC: UIViewController {
-    
-    @IBOutlet weak var slider: UISlider!
-    var sliderLabel: UILabel?
-    var valueSlider:Float = 50
+
+
+    @IBOutlet weak var valueSlider: UIView!
+    @IBOutlet weak var backgroundSlider: UIView!
+    @IBOutlet weak var thumbIcon: UIView!
+    @IBOutlet weak var valueLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        slider.transform = CGAffineTransform(rotationAngle: CGFloat(-Double.pi / 2))
-        let iconSlider = UIImage(named: "Lol_circle")
-        slider.setThumbImage(iconSlider, for: .normal)
+        setupSlider()
+        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        self.thumbIcon.addGestureRecognizer(gestureRecognizer)
+        thumbIcon.isUserInteractionEnabled = true
+        valueLabel.textAlignment = .center
+        valueLabel.text = "45"
     }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        if let handleView = slider.subviews.last as? UIImageView {
-            let label = UILabel(frame: handleView.bounds)
-            label.backgroundColor = .clear
-            label.textAlignment = .center
-            handleView.addSubview(label)
-            label.text = "50"
-            label.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi / 2))
-            self.sliderLabel = label
+
+    func setupSlider() {
+        backgroundSlider.layer.masksToBounds = true
+        backgroundSlider.layer.borderWidth = 1.0
+        backgroundSlider.layer.borderColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)
+        valueSlider.layer.masksToBounds = true
+        valueSlider.layer.borderWidth = 1.0
+        valueSlider.layer.borderColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
+        valueSlider.backgroundColor = .white
+    }
+
+    @objc func handlePan(_ gestureRecognizer: UIPanGestureRecognizer) {
+        let sliderY = self.backgroundSlider.frame.origin.y
+        let sliderHeight = self.backgroundSlider.frame.height
+        let heightValue = gestureRecognizer.view!.center.y - sliderY
+        
+        if gestureRecognizer.state == .began || gestureRecognizer.state == .changed {
+            let translation = gestureRecognizer.translation(in: self.view)
+            let gestureY = gestureRecognizer.view!.center.y + translation.y
+            if gestureY > sliderY && gestureY < (sliderY + sliderHeight) {
+                gestureRecognizer.view!.center = CGPoint(x: gestureRecognizer.view!.center.x, y: gestureRecognizer.view!.center.y + translation.y)
+                gestureRecognizer.setTranslation(CGPoint.zero, in: self.view)
+                valueSlider.frame = CGRect(x: self.backgroundSlider.frame.origin.x, y: self.backgroundSlider.frame.origin.y, width: self.backgroundSlider.frame.width, height: heightValue)
+            }
+        } else {
+            let value = ((sliderHeight - heightValue) / sliderHeight) * 100
+            let realValue = Double(round(value))
+            valueLabel.text = " \(Int(realValue))"
+            print(realValue)
         }
     }
-    @IBAction func sliderAction(_ sender: Any) {
-        self.sliderLabel!.text = "\(Int(slider.value))"
-    }
-    
-    
-}
 
+}
