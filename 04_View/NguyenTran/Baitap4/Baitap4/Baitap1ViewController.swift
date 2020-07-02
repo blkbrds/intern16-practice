ab8619a8f1bc8f2478e86ef1dc705ce06367831b
Nguyen Tran T <nguyen.tran@asiantech.vn>
2020-07-02 09:11:40 +0700

    Format code

--- 04_View/NguyenTran/Baitap4/Baitap4/Baitap1ViewController.swift
+++ 04_View/NguyenTran/Baitap4/Baitap4/Baitap1ViewController.swift
@@ -21,22 +21,22 @@ class Baitap1ViewController: UIViewController {
         userAvatar.layer.borderWidth = 1
         userAvatar.layer.borderColor = UIColor.black.cgColor
         view.addSubview(userAvatar)
-        
+
         //Add user name
-        let userName = UILabel(frame: CGRect(x:50, y:200, width: 100, height: 50))
+        let userName = UILabel(frame: CGRect(x: 50, y: 200, width: 100, height: 50))
         userName.text = "Name"
         userName.backgroundColor = #colorLiteral(red: 0.9843137255, green: 0.9333333333, blue: 0.7529411765, alpha: 1)
         userName.textColor = #colorLiteral(red: 0, green: 0.568627451, blue: 1, alpha: 1)
         userName.textAlignment = .center
         view.addSubview(userName)
-        
+
         //Add button
-        let button = UIButton(frame: CGRect(x:50, y:100, width: 100, height: 250))
+        let button = UIButton(frame: CGRect(x: 50, y: 100, width: 100, height: 250))
         button.backgroundColor = .clear
         button.addTarget(self, action: #selector(buttonDidClick), for: .touchUpInside)
         view.addSubview(button)
     }
-    
+
     @objc func buttonDidClick() {
         print("Button is clicked!")
     }
