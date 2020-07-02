7d115a805aa7ba2b12f637f2b5fefd6cfabf5b6a
hien.nguyen <hien.nguyen4@asiantech.vn>
2020-07-02 11:16:30 +0700

    HIENNGUYEN4 - Basic swift - phần 7 bài tập lần 2 Lần PR này phản ánh các phần sau: - Bài tập 4 Tổng của 100 số Fibonacy đầu tiên Liệt kê 100 số Fibonacy đầu tiên - Bài tập 5 Tìm chuỗi con trong chuỗi mẹ
    - Bài tập 7
    Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.

--- 02_SwiftBasic/HIENNGUYEN4:Baitap2_lan1/MyPlayground.playground/Contents.swift
+++ 02_SwiftBasic/HIENNGUYEN4:Baitap2_lan1/MyPlayground.playground/Contents.swift
@@ -1,36 +1,76 @@
-
-
 import UIKit
 
-//baitap 1 calculate area and volumn of a cicle
-func Ciclecalculate (to radius: Double) -> (area: Double, volumn: Double) {
-    let area: Double = 4 * Double.pi * radius * radius
-    let volumn: Double = (4.0 / 3.0) * Double.pi * radius * radius * radius
-    return (area, volumn)
+
+//tìm ra 100 số đầu tiên của mảng số sử dụng hàm vòng lặp và tính tổng
+func fib()-> [Double]{
+    
+    var f1 = 0.0
+    
+    var f2 = 1.0
+    
+    var f  = 0.0
+    
+    var araryOfFibs = [Double]()
+    
+    repeat {
+        
+        araryOfFibs.append(f)
+        
+        f = f1 + f2
+        
+        f1 = f2
+        
+        f2 = f
+        
+    } while araryOfFibs.count < 100
+
+    return araryOfFibs
 }
 
-let aa = Ciclecalculate(to: 3)
-print (aa.area, aa.volumn)
-//baitap 1 calculate area and volumn of a cicle
+func sumOf(numbers: [Double]) -> Double {
+    var sum = 0.0
+    for number in numbers {
+        sum += number
+    }
+    return sum
+}
 
-//Bai tap so 7 // hoan doi random cac phan tu trong array
-var number: [String] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-number.reverse()
-print(number)
-// print dao nguoc thu tu cac element cua array
+let fa = fib()
+let sum = sumOf(numbers: fa)
+print(fa.count)
+print(sum)
 
-let descendingnumber = number.sorted(by: <)
-print(descendingnumber)
-//sap xep cac element theo thu tu tang dan
 
-number.shuffle()
-//sap xep cac element theo random ngau nhien
+//Bài 7: Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
 
-let shuffledNumbers = number.shuffled()
-//xao tron cac element
+var arrString = ["1", "2", "3", "1", "4", "1"]
+var elemToReplace = "1"
+var substitutionElem = "3"
+for i in 0..<arrString.count {
+    if arrString[i] == elemToReplace {
+        let newString = arrString[i].replacingOccurrences(of: arrString[i], with: substitutionElem)
+        arrString[i] = newString
+    } else {
+        arrString[i] = arrString[i]
+    }
+}
+print(arrString)
+    
 
-number.swapAt(2, 3)
-//Xao tron cac element bang cach chi dinh index cua element
 
-number.swapAt(4, 5)
-//Xao tron cac element bang cach chi dinh index cua element (vi du khac)
+//bai 5: Tìm chuỗi con trong chuỗi mẹ
+let string = "cdefgvabfeghvbgggjhgabffggab"
+let substring = "ab"
+if string.contains(substring) {
+    print(substring.count)
+}
+
+
+
+
+
+
+
+
+
+
