b2021e96a7303fb16c42632300e3d20507a4beb8
PCI0004 <trin.nguyen@asiantech.vn>
2020-07-02 16:12:35 +0700

    Update bài tập 04 và bài tập 05

---
+++ 04_View/TrinNguyen/BaiTapView/BaiTapView/BaiTap04.swift
@@ -0,0 +1,38 @@
+//
+//  BaiTap04.swift
+//  BaiTapView
+//
+//  Created by PCI0004 on 7/2/20.
+//  Copyright © 2020 PCI0004. All rights reserved.
+//
+
+import UIKit
+
+final class BaiTap04: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet private weak var toadoLabel: UILabel!
+    @IBOutlet private weak var colorView: UIView!
+    @IBOutlet private weak var redSlider: UISlider!
+    @IBOutlet private weak var greenSlider: UISlider!
+    @IBOutlet private weak var blueSlider: UISlider!
+
+    // MARK: - Private Methods
+    // MARK: - Private
+    
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configTextField()
+    }
+    private func configTextField() {
+    }
+
+    // MARK: - IBActions
+    @IBAction func sliderAction(_ sender: UISlider) {
+        toadoLabel.text = "Color(R: \(redSlider.value * 255), G: \(greenSlider.value * 255),B: \(blueSlider.value * 255))"
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    // MARK: - Navigation
+    
+}
---
+++ 04_View/TrinNguyen/BaiTapView/BaiTapView/BaiTap05.swift
@@ -0,0 +1,43 @@
+//
+//  BaiTap05.swift
+//  BaiTapView
+//
+//  Created by PCI0004 on 7/2/20.
+//  Copyright © 2020 PCI0004. All rights reserved.
+//
+
+import UIKit
+
+final class BaiTap05: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet private weak var outsideView: UIView!
+    @IBOutlet private weak var whiteView: UIView!
+    @IBOutlet private weak var blueView: UIView!
+    @IBOutlet private weak var orangeView: UIView!
+    @IBOutlet private weak var numberLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        blueView.backgroundColor = .blue
+        orangeView.backgroundColor = .orange
+        whiteView.backgroundColor = .gray
+//        numberLabel.backgroundColor = .black
+        
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let position = touch.location(in: outsideView)
+            if position.y <= whiteView.frame.maxY && position.y >= whiteView.frame.minY {
+                orangeView.center = CGPoint(x: whiteView.center.x, y: position.y)
+                blueView.frame = CGRect(x: blueView.frame.origin.x, y: blueView.frame.origin.y, width: blueView.frame.width, height:
+                    position.y - whiteView.frame.origin.y)
+                numberLabel.text = "\(Int(blueView.frame.height / whiteView.frame.height * 100))"
+            }
+        }
+
+    }
+    
+
+
+}
