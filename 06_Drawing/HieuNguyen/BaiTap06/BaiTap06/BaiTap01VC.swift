b0cbf66111fcd22336c0b9634e11dd9c48ea3ae2
hieu.ngq <hieu.nguyen3@asiantech.vn>
2020-07-02 17:36:28 +0700

    -First commit

---
+++ 06_Drawing/HieuNguyen/BaiTap06/BaiTap06/BaiTap01VC.swift
@@ -0,0 +1,46 @@
+//
+//  BaiTap01VC.swift
+//  BaiTap06
+//
+//  Created by PCI0020 on 7/2/20.
+//  Copyright © 2020 hieu.ngq. All rights reserved.
+//
+
+import UIKit
+
+class BaiTap01VC: UIViewController {
+
+    @IBOutlet weak var viewGrid: UIView!
+    var data:[Int] = [98, 95, 103, 102, 99, 97, 98]
+    var gridY:[Int] = [90, 95, 100, 105]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        drawLine(color: .red, view: viewGrid, value: 95, pointX0: 0)
+    }
+    
+    func drawLine(color: UIColor, view: UIView, value: Int, pointX0: Int) {
+        let pointX0: Int = pointX0
+        let pointY0: Int = Int(view.frame.size.height)
+        
+        let height = pointY0 - Int(pointY0) / 15 * (value - 90)
+        print(pointX0)
+        print(pointY0)
+        //design the path
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: pointY0))
+        path.addLine(to: CGPoint(x: 0, y: height))
+        path.addLine(to: CGPoint(x: pointX0, y: height))
+        path.addLine(to: CGPoint(x: pointX0, y: pointY0))
+        
+        //design path in layer
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.strokeColor = color.cgColor
+        shapeLayer.lineWidth = 1.0
+
+        view.layer.addSublayer(shapeLayer)
+    }
+
+}
---
+++ 06_Drawing/HieuNguyen/BaiTap06/BaiTap06/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaiTap06
+//
+//  Created by PCI0020 on 7/2/20.
+//  Copyright © 2020 hieu.ngq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
