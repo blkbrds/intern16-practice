7e0d915fb2622678defd152a51a0b1e219a4f15e
hieu.ngq <hieu.nguyen3@asiantech.vn>
2020-07-03 17:33:54 +0700

    -Xong bai 6

---
+++ 07_Autolayout/HieuNguyen/BaiTap07/BaiTap07/Controller/BaiTap06/Bai6b.swift
@@ -0,0 +1,30 @@
+//
+//  Bai6b.swift
+//  BaiTap07
+//
+//  Created by PCI0020 on 7/3/20.
+//  Copyright © 2020 hieu.ngq. All rights reserved.
+//
+
+import UIKit
+
+class Bai6b: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
