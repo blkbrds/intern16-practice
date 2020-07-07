ea0a6a436f2ca4d9d3a0ea150b8225b502a127bc
PCI0004 <trin.nguyen@asiantech.vn>
2020-07-07 08:36:04 +0700

    Update bai tap 02

--- 03_SwiftOOP/TrinNguyen/BaiTap02.playground/Contents.swift
+++ 03_SwiftOOP/TrinNguyen/BaiTap02.playground/Contents.swift
@@ -40,15 +40,24 @@ if let daGiac = DaGiac(canh: 3, arrayCanh: [8, 6, 12]) {
 class TamGiac: DaGiac {
     // Override lai de tinh dien tich
     override init?(canh: Int, arrayCanh: [Int]) {
-        if canh == arrayCanh.count && arrayCanh[0] + arrayCanh[1] > arrayCanh[2] && arrayCanh[1] + arrayCanh [2] > 0 && arrayCanh[0] + arrayCanh [2] > arrayCanh[1] {
-            super.init(canh: canh, arrayCanh: arrayCanh)
-        } else {
+        super.init(canh: canh, arrayCanh: arrayCanh)
+        let check = checkTamGiac(canh: canh, arrayCanh: arrayCanh)
+        if !check {
             return nil
         }
     }
+
+    func checkTamGiac(canh: Int, arrayCanh: [Int]) -> Bool {
+        if canh == arrayCanh.count && arrayCanh[0] + arrayCanh[1] > arrayCanh[2] && arrayCanh[1] + arrayCanh [2] > arrayCanh[0] && arrayCanh[0] + arrayCanh [2] > arrayCanh[1] {
+            return true
+        }
+        return false
+    }
+
     func tinhChuVi() -> Float {
         Float(super.tinhChuVi())
     }
+
     func tinhDienTich() -> Float {
         var dienTich: Float = 0.0
         let p: Float = self.tinhChuVi() / 2
@@ -58,7 +67,7 @@ class TamGiac: DaGiac {
     }
 }
 
-if let tamGiac = TamGiac(canh: 3, arrayCanh: [8, 4, 11]) {
+if let tamGiac = TamGiac(canh: 3, arrayCanh: [8, 4, 13]) {
     print("Dien tich cua tam giac la: \(tamGiac.tinhDienTich())")
 } else {
     print("Nhap sai vui long nhap lai")
