fac76fa0294abb2ab4f44414060736a426571773
Trung Le D <trung.le@asiantech.vn>
2020-07-09 15:35:13 +0700

    TrungleD - Bai Tap Autolayout - Bai 1 den bai 3

---
+++ 07_Autolayout/TRUNGLE/Autolayout/Autolayout/Bai3-3/Bai3_2ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Bai3_2ViewController.swift
+//  Autolayout
+//
+//  Created by Trung Le D. on 7/9/20.
+//  Copyright © 2020 Trung Le D. All rights reserved.
+//
+
+import UIKit
+
+class Bai3_2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
