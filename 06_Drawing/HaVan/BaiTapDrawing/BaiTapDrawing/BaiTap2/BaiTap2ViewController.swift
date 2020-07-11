fd7c38aea579c3e5f80038b625f7fabe7b772738
AT_HaVan <ha.van@asiantech.vn>
2020-07-11 10:25:42 +0700

    Fix position của circle view - bài 2

--- 06_Drawing/HaVan/BaiTapDrawing/BaiTapDrawing/BaiTap2/BaiTap2ViewController.swift
+++ 06_Drawing/HaVan/BaiTapDrawing/BaiTapDrawing/BaiTap2/BaiTap2ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 final class BaiTap2ViewController: UIViewController {
-
+    
     // MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        let circle = CircleGraphic(frame: CGRect(x: 10, y: 100, width: 300, height: 300), percent: 60)
+        configCircleView()
+    }
+    
+    //MARK: -Private functions
+    private func configCircleView() {
+        let screenSize = UIScreen.main.bounds.size
+        let widthCircle: CGFloat = 300
+        let circle = CircleGraphic(frame: CGRect(x: (screenSize.width - widthCircle) / 2, y: (screenSize.height - widthCircle) / 2, width: widthCircle, height: widthCircle), percent: 60)
         view.addSubview(circle)
     }
-
+    
 }
