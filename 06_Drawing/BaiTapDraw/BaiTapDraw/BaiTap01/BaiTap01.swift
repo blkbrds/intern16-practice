d9d45b1e688bb31b8ca550528ac3702817b6834a
PCI0004 <trin.nguyen@asiantech.vn>
2020-07-14 09:22:28 +0700

    Commit Bai Tap 1&2

---
+++ 06_Drawing/BaiTapDraw/BaiTapDraw/BaiTap01/BaiTap01.swift
@@ -0,0 +1,69 @@
+//
+//  BaiTap01.swift
+//  BaiTapDraw
+//
+//  Created by PCI0004 on 7/9/20.
+//  Copyright © 2020 PCI0004. All rights reserved.
+//
+
+import UIKit
+
+final class BaiTap01: UIViewController {
+
+    // MARK: - Propeties
+    var columnX = 30
+    var columnY = 550
+
+    struct Chart {
+        var heightY: Int
+        var name: String
+    }
+
+    let charts: [Chart] = [
+        Chart(heightY: 200, name: "Monday"),
+        Chart(heightY: 250, name: "Tuesday"),
+        Chart(heightY: 150, name: "Wednesday"),
+        Chart(heightY: 300, name: "Thusday"),
+        Chart(heightY: 225, name: "Friday")
+    ]
+
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        drawChart()
+    }
+
+    // MARK: - Private Function
+    private func random() -> UIColor {
+        return UIColor(red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1),
+            alpha: 1.0)
+    }
+
+    private func drawChart() {
+
+        // Draw OXY
+        let lineX = ChartLine(frame: CGRect(x: 30, y: 200, width: 3, height: 300),
+            backgroundColor: UIColor.black.cgColor)
+        let lineY = ChartLine(frame: CGRect(x: lineX.frame.origin.x, y: lineX.frame.maxY, width: 350, height: 3),
+            backgroundColor: UIColor.black.cgColor)
+        view.addSubview(lineX)
+        view.addSubview(lineY)
+
+        let width: Int = 30
+        let space: Int = 30
+
+        // Draw columns
+        for (index, chart) in charts.enumerated() {
+            let x: Int = Int(lineX.frame.origin.x) + space + index * space + index * width
+            let y: Int = Int(lineY.frame.origin.y) - chart.heightY + Int(ChartColume.heightLabel)
+            let column = ChartColume(frame: CGRect(x: x, y: y, width: width, height: chart.heightY),
+                name: chart.name,
+                backgroundColor: random().cgColor,
+                nameColor: .black)
+            view.addSubview(column)
+        }
+    }
+}
---
+++ 06_Drawing/BaiTapDraw/BaiTapDraw/BaiTap02/BaiTap02.swift
@@ -0,0 +1,40 @@
+//
+//  BaiTap02.swift
+//  BaiTapDraw
+//
+//  Created by PCI0004 on 7/13/20.
+//  Copyright © 2020 PCI0004. All rights reserved.
+//
+
+import UIKit
+
+final class BaiTap02: UIViewController {
+
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        drawChart()
+
+    }
+
+    // MARK: - Private Function
+    private func drawChart() {
+
+        // Draw OXY
+        let frameX = CGRect(x: 30, y: 250, width: 3, height: 300)
+        let lineX = DrawLine(frame: frameX, backgroundColor: UIColor.black.cgColor)
+        let frameY = CGRect(x: lineX.frame.origin.x, y: lineX.frame.maxY, width: 350, height: 3)
+        let lineY = DrawLine(frame: frameY, backgroundColor: UIColor.black.cgColor)
+        view.addSubview(lineX)
+        view.addSubview(lineY)
+
+        // Draw Circle
+        let frame1 = CGRect(x: 50, y: 250, width: 300, height: 300)
+        let circleCustom = CircleCustom(frame: frame1)
+        circleCustom.values = [1, 2, 3, 4]
+        circleCustom.createCircle()
+        view.addSubview(circleCustom)
+
+    }
+}
