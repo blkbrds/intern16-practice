83764a27af44e96a57333358393ff72789ee7dc2
minh.nguyen T[4] <minh.nguyen4@asiantech.vn>
2020-07-15 00:50:41 +0700

    Bai 5a,6a

---
+++ 07_Autolayout/MINHNGUYEN T/BaiTap BaiTapAutoLAyout/BaiTap BaiTapAutoLAyout/BaiTap6/BaiTap6aViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BaiTap6aViewController.swift
+//  BaiTap BaiTapAutoLAyout
+//
+//  Created by MacBook Pro on 7/14/20.
+//  Copyright © 2020 asiantech. All rights reserved.
+//
+
+import UIKit
+
+class BaiTap6aViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- 07_Autolayout/MINHNGUYEN T/BaiTap BaiTapAutoLAyout/BaiTap BaiTapAutoLAyout/SceneDelegate.swift
+++ 07_Autolayout/MINHNGUYEN T/BaiTap BaiTapAutoLAyout/BaiTap BaiTapAutoLAyout/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = BaiTap4eViewController()
+        window.rootViewController = BaiTap5bViewController()
         self.window = window
         window.makeKeyAndVisible()
     }
