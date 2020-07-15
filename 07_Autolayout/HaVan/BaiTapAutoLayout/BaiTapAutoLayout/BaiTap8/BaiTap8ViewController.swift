faacb51bc696898b9d777d3d238e17e57be0fde8
AT_HaVan <ha.van@asiantech.vn>
2020-07-15 23:07:18 +0700

    Xong bài tập 8 + Fix bug bài 6'' và 5''

---
+++ 07_Autolayout/HaVan/BaiTapAutoLayout/BaiTapAutoLayout/BaiTap8/BaiTap8ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BaiTap8ViewController.swift
+//  BaiTapAutoLayout
+//
+//  Created by NganHa on 7/15/20.
+//  Copyright © 2020 Galaxy. All rights reserved.
+//
+
+import UIKit
+
+class BaiTap8ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
