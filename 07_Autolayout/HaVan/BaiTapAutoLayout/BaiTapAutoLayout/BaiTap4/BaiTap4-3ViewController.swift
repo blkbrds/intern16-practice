fd0776915d20baf7b4b89956c2fa73d41f641b0e
AT_HaVan <ha.van@asiantech.vn>
2020-07-15 15:47:31 +0700

    Xong bài tập 3 -> 6

---
+++ 07_Autolayout/HaVan/BaiTapAutoLayout/BaiTapAutoLayout/BaiTap4/BaiTap4-3ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaiTap4-3ViewController.swift
+//  BaiTapAutoLayout
+//
+//  Created by NganHa on 7/15/20.
+//  Copyright © 2020 Galaxy. All rights reserved.
+//
+
+import UIKit
+
+class BaiTap4_3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 07_Autolayout/HaVan/BaiTapAutoLayout/BaiTapAutoLayout/BaiTap5/BaiTap5-4ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaiTap5-4ViewController.swift
+//  BaiTapAutoLayout
+//
+//  Created by NganHa on 7/15/20.
+//  Copyright © 2020 Galaxy. All rights reserved.
+//
+
+import UIKit
+
+class BaiTap5_4ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
