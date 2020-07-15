88c519876954c547d51f3e58c1c57c3445c0105c
hieu.ngq <hieu.nguyen3@asiantech.vn>
2020-07-15 17:24:46 +0700

    xong bai 12.1 12.2

---
+++ 12_CollectionView/HieuNguyen/BaiTap12/BaiTap12/BaiTap02/ExerciseTwoViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ExerciseTwoViewController.swift
+//  BaiTap12
+//
+//  Created by PCI0020 on 7/15/20.
+//  Copyright © 2020 hieu.ngq. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseTwoViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configCollectionView()
+    }
+    
+    private func configCollectionView() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        if let layout = collectionView.collectionViewLayout as? CustomLayout{
+            layout.layoutDelegate = self
+        }
+    }
+}
+
+extension ExerciseTwoViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, CustomLayoutDelegate {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func heightFor(index: Int) -> CGFloat {
+        return CGFloat(Float.random(in: 50..<200))
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        let label = UILabel()
+        label.text = String(indexPath.row)
+        label.backgroundColor = .blue
+        label.textColor = .white
+        label.textAlignment = .center
+        label.frame = cell.bounds
+        cell.addSubview(label)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    
+}
