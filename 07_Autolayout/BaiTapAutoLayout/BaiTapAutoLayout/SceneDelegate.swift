8990b91154f15a2326b3eb75df65a0716b75c9ac
PCI0004 <trin.nguyen@asiantech.vn>
2020-07-16 08:56:01 +0700

    Commit Bai8,9 Phan AutoLayout

--- 07_Autolayout/BaiTapAutoLayout/BaiTapAutoLayout/SceneDelegate.swift
+++ 07_Autolayout/BaiTapAutoLayout/BaiTapAutoLayout/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = BaiTap01()
+        window.rootViewController = BaiTap09A()
         self.window = window
         window.makeKeyAndVisible()
 
