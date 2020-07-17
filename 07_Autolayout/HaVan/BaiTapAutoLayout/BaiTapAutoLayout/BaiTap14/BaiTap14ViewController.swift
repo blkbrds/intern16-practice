072a9ad44b4babfc5e91df103175272991a2c4ed
AT_HaVan <ha.van@asiantech.vn>
2020-07-17 15:26:22 +0700

    Xong bài 14

---
+++ 07_Autolayout/HaVan/BaiTapAutoLayout/BaiTapAutoLayout/BaiTap14/BaiTap14ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BaiTap14ViewController.swift
+//  BaiTapAutoLayout
+//
+//  Created by NganHa on 7/17/20.
+//  Copyright © 2020 Galaxy. All rights reserved.
+//
+
+import UIKit
+
+class BaiTap14ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
