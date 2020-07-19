65d17b13c09cbde3ae6a60083de37e4445449b04
minh.nguyen T[4] <minh.nguyen4@asiantech.vn>
2020-07-20 00:51:07 +0700

    Hoàn thanh bài tap phan 7

---
+++ 07_Autolayout/MINHNGUYEN T/BaiTap BaiTapAutoLAyout/BaiTap BaiTapAutoLAyout/BaiTap13/BaiTap13ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BaiTap13ViewController.swift
+//  BaiTap BaiTapAutoLAyout
+//
+//  Created by MacBook Pro on 7/17/20.
+//  Copyright © 2020 asiantech. All rights reserved.
+//
+
+import UIKit
+
+class BaiTap13ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
