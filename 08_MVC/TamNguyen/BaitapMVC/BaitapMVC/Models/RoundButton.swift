5bca258e5ac8f174fd5dadf871699a6c24ce48a3
Tam Nguyen K <tam.nguyen7@asiantech.vn>
2020-07-20 23:11:59 +0700

    Bổ sung

---
+++ 08_MVC/TamNguyen/BaitapMVC/BaitapMVC/Models/RoundButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundButton.swift
+//  BaitapMVC
+//
+//  Created by PCI0001 on 7/20/20.
+//  Copyright © 2020 PCI0001. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
