7f0e411a5bb1cfea7deda0d9f3c161d5c2446123
at-trungle <trung.le@asiantech.vn>
2020-07-22 06:44:32 +0700

    Bai 2 - navi

--- 09_Navigation/TRUNGLE/Bai2Navigation/Bai2Navigation/View/LoginViewController.swift
+++ 09_Navigation/TRUNGLE/Bai2Navigation/Bai2Navigation/View/LoginViewController.swift
@@ -10,14 +10,58 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var passWordTextField: UITextField!
-    @IBOutlet weak var userNameTextField: UITextField!
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  // MARK: - Properties
+        enum Notifications: String {
+            case fullEmpty = "Yêu cầu bạn nhập đầy đủ"
+            case halfEmpty = "Bạn nhập thiếu "
+            case wrongInformation = " Không đúng"
+        }
         
-    }
-    
-    func getData() {
+        // MARK: - IBOulets
+        @IBOutlet weak var informationLabel: UILabel!
+        @IBOutlet weak var userNameTextField: UITextField!
+        @IBOutlet weak var passwordTextField: UITextField!
         
-    }
-}
+        // MARK: - Life cycle
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            configUI()
+        }
+        
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(true)
+            userNameTextField.text = ""
+            passwordTextField.text = ""
+            informationLabel.text = ""
+        }
+        
+        // MARK: - Private methods
+        private func configUI() {
+            title = "Login"
+            let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(pushHomeViewTouchUpInside))
+            navigationItem.rightBarButtonItem = doneButton
+        }
+        
+        @objc private func pushHomeViewTouchUpInside() {
+            if let path = Bundle.main.path(forResource: "user", ofType: "plist") {
+                if let dic = NSDictionary(contentsOfFile: path) as? [String:String] {
+                    for account in dic {
+//                        let result: (String?, String?) = (userNameTextField.text, passwordTextField.text)
+//                        switch result {
+//                        case ("",""):
+//                            informationLabel.text = Notifications.fullEmpty.rawValue
+//                        case ("", _), (_, ""):
+//                            informationLabel.text = Notifications.halfEmpty.rawValue
+//                        case (account.key, account.value):
+//                            let nextHomeView = HomeViewController()
+//                            navigationController?.pushViewController(nextHomeView, animated: true)
+//                        default:
+//                            informationLabel.text = Notifications.wrongInformation.rawValue
+                        print(account)
+                        
+                        }
+                    }
+                }
+            }
+        }
+    //}
