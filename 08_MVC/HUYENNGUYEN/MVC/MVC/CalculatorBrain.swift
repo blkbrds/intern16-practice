109902ecd37492515c9a9c402e672aa9985cae52
huyen.nguyen2 <huyen.nguyen2@asiantech.vn>
2020-07-21 07:13:56 +0700

    Create model, handle logic Calculator

--- 08_MVC/HUYENNGUYEN/MVC/MVC/CalculatorBrain.swift
+++ 08_MVC/HUYENNGUYEN/MVC/MVC/CalculatorBrain.swift
@@ -1,3 +1,63 @@
 //  CalculatorBrain.swift
 //  MVC
 import Foundation
+
+struct CalculatorBrain {
+    var result : Double? {
+        didSet {
+            if result != nil {
+                opera = result
+            }
+        }
+    }
+    var opera: Double?
+    private var pendingOperation: PendingBinaryOperation?
+
+    //set phep tinh
+    mutating func setOpera(_ opera: Double) {
+        self.opera = opera
+        result = opera
+    }
+    
+    mutating func performOperation(_ symbol: String) {
+        
+        let operation = operations[symbol]! //lay gia tri cua phep tinh
+        
+        switch operation {
+        case .binary(let function):
+            //neu la binary thi luu so da nhap va phep tinh vao
+            if opera != nil {
+                pendingOperation = PendingBinaryOperation(firstOperand: opera!, operation: function)
+                result = nil
+            }
+        case .equal:
+            if pendingOperation != nil && opera != nil {
+                result = pendingOperation?.perform(with: opera!)
+            }
+        }
+    }
+    
+    // luu tru toan tu va hang
+    struct PendingBinaryOperation {
+        let firstOperand: Double
+        let operation: (Double, Double) -> Double
+        func perform(with secondOperand: Double) -> Double {
+            return operation(firstOperand,secondOperand)
+        }
+    }
+    
+    // enum xac dinh cac phep tinh
+    enum Operation {
+        case binary((Double,Double) -> Double)
+        case equal
+    }
+    
+    //gan co dinh cac phep tinh
+    var operations: [String: Operation] = [
+        "+" :   .binary( + ),
+        "−" :   .binary( - ),
+        "×" :   .binary( * ),
+        "/" :   .binary( / ),
+        "=" :   .equal,
+    ]
+}
--- 08_MVC/HUYENNGUYEN/MVC/MVC/CalculatorViewController.swift
+++ 08_MVC/HUYENNGUYEN/MVC/MVC/CalculatorViewController.swift
@@ -6,10 +6,27 @@ class CalculatorViewController: UIViewController {
     
     //MARK: - IBOutlet
     @IBOutlet weak var digitLabel: UILabel!
+    
+    //MARK: - Properties
     var isDigitInLabel: Bool = false
     var previousNumber: Double = 0
     var numberOnLabel: Double = 0
     var operation = 0
+    
+    var displayValue: Double {
+        get {
+            return Double(digitLabel.text!)!
+        }
+        set {
+            if Double(Int(newValue)) == newValue {
+                digitLabel.text = String(Int(newValue))
+            }else {
+                digitLabel.text = String(newValue)
+            }
+        }
+    }
+    
+    var brain = CalculatorBrain()
     
     //MARK: - Life Cycle
     override func viewDidLoad() {
@@ -19,12 +36,20 @@ class CalculatorViewController: UIViewController {
     
     @IBAction func numberButtonAction(_ sender: UIButton) {
         if isDigitInLabel == true {
-            digitLabel.text = String(sender.tag)
-            numberOnLabel = Double(digitLabel.text!)!
-            isDigitInLabel = false
+            if sender.currentTitle == "0" && digitLabel.text! == "0" {
+                digitLabel.text = "0"
+            }
+            else {
+                digitLabel.text = digitLabel.text! + sender.currentTitle!
+            }
+//            digitLabel.text = String(sender.tag)
+//            numberOnLabel = Double(digitLabel.text!)!
+            //isDigitInLabel = false
         } else {
-            digitLabel.text = digitLabel.text! +  String(sender.tag)
-            numberOnLabel = Double(digitLabel.text!)!
+            digitLabel.text = sender.currentTitle
+            isDigitInLabel = true
+//            digitLabel.text = digitLabel.text! +  String(sender.tag)
+//            numberOnLabel = Double(digitLabel.text!)!
         }
         //        let digit = sender.currentTitle!
         //        let numberInput = digitLabel.text!
@@ -39,47 +64,63 @@ class CalculatorViewController: UIViewController {
     
     @IBAction func deleteButtonAction(_ sender: UIButton) {
         digitLabel.text = "0"
-        previousNumber = 0
-        numberOnLabel = 0
-        operation = 0
+//        previousNumber = 0
+//        numberOnLabel = 0
+//        operation = 0
     }
     
     @IBAction func operatorButtonAction(_ sender: UIButton) {
-        // digitLabel.text =
-        //previousNumber = Double(digitLabel.text!)!
-        if sender.tag != 11 && sender.tag != 16 && digitLabel.text != "" {
-            previousNumber = Double(digitLabel.text!)!
-            if sender.tag == 12 {
-                digitLabel.text = digitLabel.text! + ":"
-                operation = 1
-            }
-            if sender.tag == 13 {
-                digitLabel.text = digitLabel.text! + "+"
-                operation = 2
-            }
-            if sender.tag == 14 {
-                digitLabel.text = digitLabel.text! + "-"
-                operation = 3
-            }
-            if sender.tag == 15 {
-                //sender.currentTitle
-                digitLabel.text = digitLabel.text! + "x"
-                operation = 4
-            }
-            isDigitInLabel = true
-        } else {
-            if sender.tag == 11 {
-                switch operation {
-                case 1:
-                     digitLabel.text = String(previousNumber / numberOnLabel)
-                case 2:
-                    digitLabel.text = String(previousNumber + numberOnLabel)
-                case 3:
-                    digitLabel.text = String(previousNumber - numberOnLabel)
-                default:
-                     digitLabel.text = String(previousNumber * numberOnLabel)
-                }
-            }
+        if isDigitInLabel {
+            brain.setOpera(displayValue)
+            isDigitInLabel = false
         }
+        
+        brain.performOperation(sender.currentTitle!)
+        
+        if let result = brain.result {
+            displayValue = result
+        }
+        // digitLabel.text =
+//        //previousNumber = Double(digitLabel.text!)!
+//        if sender.tag != 11 && sender.tag != 16 && digitLabel.text != "" {
+//            previousNumber = Double(digitLabel.text!)!
+//            if sender.tag == 12 {
+//                digitLabel.text = digitLabel.text! + ":"
+//                operation = 1
+//            }
+//            if sender.tag == 13 {
+//                digitLabel.text = digitLabel.text! + "+"
+//                operation = 2
+//            }
+//            if sender.tag == 14 {
+//                digitLabel.text = digitLabel.text! + "-"
+//                operation = 3
+//            }
+//            if sender.tag == 15 {
+//                sender.currentTitle
+//                digitLabel.text = digitLabel.text! + "x"
+//                operation = 4
+//            }
+//            isDigitInLabel = true
+//        } else {
+//            if sender.tag == 11 {
+//                switch operation {
+//                case 1:
+//                     digitLabel.text = String(previousNumber / numberOnLabel)
+//                case 2:
+//                    digitLabel.text = String(previousNumber + numberOnLabel)
+//                case 3:
+//                    digitLabel.text = String(previousNumber - numberOnLabel)
+//                default:
+//                     digitLabel.text = String(previousNumber * numberOnLabel)
+//                }
+//            }
+//        }
     }
 }
+//
+//extension CalculatorViewController {
+//    override var preferredStatusBarStyle: UIStatusBarStyle {
+//        return .lightContent
+//    }
+//}
