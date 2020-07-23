af6326fc26faee3060344c2fe742322f20cf5c00
Tam Nguyen K <tam.nguyen7@asiantech.vn>
2020-07-23 08:34:51 +0700

    Commit bai 3

---
+++ 09_Navigation/TamNguyen/BaitapNavigation/BaitapNavigation/Bai3/Avatar.swift
@@ -0,0 +1,21 @@
+//
+//  Avatar.swift
+//  BaitapNavigation
+//
+//  Created by PCI0001 on 7/22/20.
+//  Copyright © 2020 PCI0001. All rights reserved.
+//
+
+import UIKit
+
+class Avatar: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
