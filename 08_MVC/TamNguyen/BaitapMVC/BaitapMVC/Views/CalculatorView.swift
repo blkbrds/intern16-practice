f270c783d71d0eb4808c928c68434749b79d31fd
Tam Nguyen K <tam.nguyen7@asiantech.vn>
2020-07-29 17:42:01 +0700

    Tao enum define tag button

--- 08_MVC/TamNguyen/BaitapMVC/BaitapMVC/Views/CalculatorView.swift
+++ 08_MVC/TamNguyen/BaitapMVC/BaitapMVC/Views/CalculatorView.swift
@@ -15,6 +15,14 @@ final class CalculatorView: UIView {
     private var previousNumber: Float = 0
     private var performMath: Bool = false
     private var operation: Int = 0
+    enum Operand: Int {
+        case add = 15
+        case subtract = 14
+        case multiply = 13
+        case divide = 12
+        case equal = 16
+        case clear = 11
+    }
     
     // MARK: - IBOulet
     @IBOutlet private weak var currentValueFloat: UILabel!
@@ -34,7 +42,7 @@ final class CalculatorView: UIView {
     
     @IBAction private func clickCalculatorTouchUpInside(_ sender: UIButton) {
         guard let valueText = currentValueFloat.text else { return }
-        if !valueText.isEmpty && sender.tag != 11 && sender.tag != 16 {
+        if !valueText.isEmpty && sender.tag != Operand.clear.rawValue && sender.tag != Operand.equal.rawValue {
             //devide
             switch sender.tag {
             case 12:
@@ -50,7 +58,7 @@ final class CalculatorView: UIView {
             }
             performMath = true
             operation = sender.tag
-        } else if sender.tag == 16 {
+        } else if sender.tag == Operand.equal.rawValue {
             let calculator = Calculator()
             switch operation {
             case 12:
@@ -64,7 +72,7 @@ final class CalculatorView: UIView {
                 guard let textValue = currentValueFloat.text, let valueLabel = Float(textValue) else { return }
                 previousNumber = valueLabel
             }
-        } else if sender.tag == 11 {
+        } else if sender.tag == Operand.clear.rawValue {
             currentValueFloat.text = ""
             previousNumber = 0
             numberOnScreen = 0
