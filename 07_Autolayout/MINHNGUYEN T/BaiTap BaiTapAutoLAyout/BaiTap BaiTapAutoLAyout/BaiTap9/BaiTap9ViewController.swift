89582fe269969b83869e1ddaa63cb272e2c4f5ec
minh.nguyen T[4] <minh.nguyen4@asiantech.vn>
2020-07-29 15:13:55 +0700

    Fix bai 9

--- 07_Autolayout/MINHNGUYEN T/BaiTap BaiTapAutoLAyout/BaiTap BaiTapAutoLAyout/BaiTap9/BaiTap9ViewController.swift
+++ 07_Autolayout/MINHNGUYEN T/BaiTap BaiTapAutoLAyout/BaiTap BaiTapAutoLAyout/BaiTap9/BaiTap9ViewController.swift
@@ -17,53 +17,58 @@ final class BaiTap9ViewController: UIViewController {
     @IBOutlet private weak var likeButton: UIButton!
     @IBOutlet private weak var commentButton: UIButton!
     @IBOutlet private weak var shareButton: UIButton!
-
+    
     // MARK: - Properties
-    private let data: Data = Data()
     private var location: Int = 0
-
+    private var data: [Data] = [
+        Data(nameImage: "image1", descriptionImage: "Chú mèo mướp không lớn lắm, bộ lông mượt như nhung. Đầu chú tròn và nhỏ. Ai làm gì thì hai tai dỏng lên nghe ngóng. Em thích nhất đôi mắt tròn sáng ngời như hai viên ngọc."),
+        Data(nameImage: "image2", descriptionImage: "Chú mèo có bộ lông tuyệt đẹp màu vàng óng. Đầu chú to như cái xuyến. Hai tai chú vểnh lên lúc nào cũng như nghe ngóng mọi sự việc.Chú có đôi mắt mầu vàng long lanh như hai giọt nước, buổi tối chú nhìn mọi vật rất rõ. Bạn nhìn mũi chú bé thế thôi nhưng cực thính, mũi chú thính vì chú có bộ ria em coi bộ ria của chú như cái ra đa. Mồm chú to nên lúc ăn cái gì cũng kêu meo meo. Chú khoác trên mình một bộ lông mượt óng."),
+        Data(nameImage: "image3", descriptionImage: "con mèo chỉ bằng chai nước khoáng nhỏ. Nay nó đã to phải bằng cái chai Cô-ca đại rồi. Toàn thân chú được bao phủ một màu vàng, điểm thêm và vệt trắng làm cho chiếc áo của chú lại càng thêm đẹp. Cái đầu của chú to hơn quả bóng ten-nít một chút. Đôi mắt tròn như hai hòn bi ve và sáng như đèn pha. Cái mũi phơn phớt hồng, lúc nào cũng ươn ướt như người bị cúm sổ mũi vậy"),
+        Data(nameImage: "image4", descriptionImage: "con mèo chỉ bằng chai nước khoáng nhỏ. Nay nó đã to phải bằng cái chai Cô-ca đại rồi. Toàn thân chú được bao phủ một màu vàng, điểm thêm và vệt trắng làm cho chiếc áo của chú lại càng thêm đẹp. Cái đầu của chú to hơn quả bóng ten-nít một chút. Đôi mắt tròn như hai hòn bi ve và sáng như đèn pha. Cái mũi phơn phớt hồng, lúc nào cũng ươn ướt như người bị cúm sổ mũi vậy"),
+        Data(nameImage: "image5", descriptionImage: "con mèo chỉ bằng chai nước khoáng nhỏ. Nay nó đã to phải bằng cái chai Cô-ca đại rồi. Toàn thân chú được bao phủ một màu vàng, điểm thêm và vệt trắng làm cho chiếc áo của chú lại càng thêm đẹp. Cái đầu của chú to hơn quả bóng ten-nít một chút. Đôi mắt tròn như hai hòn bi ve và sáng như đèn pha. Cái mũi phơn phớt hồng, lúc nào cũng ươn ướt như người bị cúm sổ mũi vậy")]
+    
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configButton()
         cofigDescription()
     }
-
+    
     // MARK: - Private functions
     private func cofigDescription() {
-        if data.nameImage.count == 0 {
+        if data[0].nameImage.count == 0 {
             numberOfImageLabel.text = ""
             return
         }
-        numberOfImageLabel.text = "1 / \(data.nameImage.count)"
-        imageImageView.image = UIImage(named: data.nameImage[0])
-        descriptionImageLabel.text = data.descriptionImage[0]
+        numberOfImageLabel.text = "1 / \(data.count)"
+        imageImageView.image = UIImage(named: data[0].nameImage)
+        descriptionImageLabel.text = data[0].descriptionImage
     }
-
+    
     private func configButton() {
         likeButton.layer.cornerRadius = 5
         commentButton.layer.cornerRadius = 5
         shareButton.layer.cornerRadius = 5
     }
-
+    
     // MARK: - @IBActions
     @IBAction private func nextButtonTouchInside(_ sender: UIButton) {
         location += 1
-        if location == data.nameImage.count {
+        if location == data.count {
             location = 0
         }
-        numberOfImageLabel.text = "\(location + 1) / \(data.nameImage.count)"
-        imageImageView.image = UIImage(named: data.nameImage[location])
-        descriptionImageLabel.text = data.descriptionImage[location]
+        numberOfImageLabel.text = "\(location + 1) / \(data.count)"
+        imageImageView.image = UIImage(named: data[location].nameImage)
+        descriptionImageLabel.text = data[location].descriptionImage
     }
     
     @IBAction private func backButtonTouchUpInside(_ sender: UIButton) {
         location -= 1
         if location < 0 {
-            location = data.nameImage.count - 1
+            location = data.count - 1
         }
-        numberOfImageLabel.text = "\(location + 1) / \(data.nameImage.count)"
-        imageImageView.image = UIImage(named: data.nameImage[location])
-        descriptionImageLabel.text = data.descriptionImage[location]
+        numberOfImageLabel.text = "\(location + 1) / \(data.count)"
+        imageImageView.image = UIImage(named: data[location].nameImage)
+        descriptionImageLabel.text = data[location].descriptionImage
     }
 }
--- 07_Autolayout/MINHNGUYEN T/BaiTap BaiTapAutoLAyout/BaiTap BaiTapAutoLAyout/BaiTap9/Data.swift
+++ 07_Autolayout/MINHNGUYEN T/BaiTap BaiTapAutoLAyout/BaiTap BaiTapAutoLAyout/BaiTap9/Data.swift
@@ -6,9 +6,7 @@
 //  Copyright © 2020 asiantech. All rights reserved.
 //
 
-import UIKit
-
 struct Data {
-    let nameImage: [String] = ["image1", "image2" , "image3" , "image4", "image5"]
-    let descriptionImage: [String] = ["Chú mèo mướp không lớn lắm, bộ lông mượt như nhung. Đầu chú tròn và nhỏ. Ai làm gì thì hai tai dỏng lên nghe ngóng. Em thích nhất đôi mắt tròn sáng ngời như hai viên ngọc.","Chú mèo có bộ lông tuyệt đẹp màu vàng óng. Đầu chú to như cái xuyến. Hai tai chú vểnh lên lúc nào cũng như nghe ngóng mọi sự việc.Chú có đôi mắt mầu vàng long lanh như hai giọt nước, buổi tối chú nhìn mọi vật rất rõ. Bạn nhìn mũi chú bé thế thôi nhưng cực thính, mũi chú thính vì chú có bộ ria em coi bộ ria của chú như cái ra đa. Mồm chú to nên lúc ăn cái gì cũng kêu meo meo. Chú khoác trên mình một bộ lông mượt óng.","con mèo chỉ bằng chai nước khoáng nhỏ. Nay nó đã to phải bằng cái chai Cô-ca đại rồi. Toàn thân chú được bao phủ một màu vàng, điểm thêm và vệt trắng làm cho chiếc áo của chú lại càng thêm đẹp. Cái đầu của chú to hơn quả bóng ten-nít một chút. Đôi mắt tròn như hai hòn bi ve và sáng như đèn pha. Cái mũi phơn phớt hồng, lúc nào cũng ươn ướt như người bị cúm sổ mũi vậy","Dưới bàn chân là một lớp thịt dày, mịn, màu hồng nhạt. Bà em bảo những miếng thịt đó giúp Miu di chuyển nhẹ nhàng, không gây một tiếng động nhỏ, làm cho nhiều chú chuột không ngờ. Những chiếc vuốt của chú rất nhọn và sắc.","“Meo...meo...meo, rửa mặt như mèo”. Đó là bài hát yêu thích của em Phượng, em gái em.Vì ngày nào Phượng cũng hát bài đó nên mẹ đã mua cho hai chị em một con mèo tam thể rất đẹp."]
+    var nameImage: String
+    var descriptionImage: String
 }
