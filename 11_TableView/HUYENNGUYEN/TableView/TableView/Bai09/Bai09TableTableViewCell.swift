d5a507e964d73a6548b06c0b047520bef529dc84
huyen.nguyen2 <huyen.nguyen2@asiantech.vn>
2020-08-03 20:16:43 +0700

    Create exercise 09

---
+++ 11_TableView/HUYENNGUYEN/TableView/TableView/Bai09/Bai09TableTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Bai09TableTableViewCell.swift
+//  TableView
+//
+//  Created by bu on 8/3/20.
+//  Copyright © 2020 bu. All rights reserved.
+//
+
+import UIKit
+
+class Bai09TableTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ 11_TableView/HUYENNGUYEN/TableView/TableView/Bai09/Bai09ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Bai09ViewController.swift
+//  TableView
+//
+//  Created by bu on 8/3/20.
+//  Copyright © 2020 bu. All rights reserved.
+//
+
+import UIKit
+
+class Bai09ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
