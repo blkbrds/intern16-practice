24270a2ad8560ae23eb04fefa0bdc25590d41187
huyen.nguyen2 <huyen.nguyen2@asiantech.vn>
2020-08-05 15:09:21 +0700

    Create project Collection View

---
+++ 12_CollectionView/HUYENNGUYEN/CollectionView/CollectionView/SceneDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  SceneDelegate.swift
+//  CollectionView
+//
+//  Created by bu on 8/5/20.
+//  Copyright © 2020 bu. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) { }
+
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+
+    func sceneWillResignActive(_ scene: UIScene) { }
+
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+
+    func sceneDidEnterBackground(_ scene: UIScene) { }
+}
+
