009b58eb6c05f80781a1c29354543e6a3c7b6587
huyen.nguyen2 <huyen.nguyen2@asiantech.vn>
2020-08-05 17:05:32 +0700

    Complete exercise 01

---
+++ 12_CollectionView/HUYENNGUYEN/CollectionView/CollectionView/Bai 01/Bai01ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  Bai01ViewController.swift
+//  CollectionView
+//
+//  Created by bu on 8/5/20.
+//  Copyright © 2020 bu. All rights reserved.
+//
+
+import UIKit
+
+class Bai01ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configNavigation()
+        configCollectionView()
+    }
+    
+    private func configNavigation() {
+        title = "Home"
+    }
+    
+    private func configCollectionView() {
+        let nib = UINib(nibName: "Bai01CollectionViewCell", bundle: .main)
+        collectionView.register(nib, forCellWithReuseIdentifier: "cell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+extension Bai01ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? Bai01CollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.backgroundColor = .black
+        cell.setLabel(name: "\(indexPath.item)")
+        return cell
+    }
+}
--- 12_CollectionView/HUYENNGUYEN/CollectionView/CollectionView/SceneDelegate.swift
+++ 12_CollectionView/HUYENNGUYEN/CollectionView/CollectionView/SceneDelegate.swift
@@ -14,7 +14,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene:windowScene)
+        
+        let vc = Bai01ViewController()
+        
+        let navi = UINavigationController(rootViewController: vc)
+        window.rootViewController = navi
+        
+        self.window = window
+        window.makeKeyAndVisible()        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) { }
