b5ee225e67fb8ed7c83650abed3d0b0932f00af0
at-trungle <trung.le@asiantech.vn>
2020-08-13 11:40:05 +0700

    TrungLeD - Tabbar Controller - Bai 1

---
+++ 13_TabbarController/TRUNGLE/TabbarController/TabbarController/Profile/ProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.swift
+//  TabbarController
+//
+//  Created by Abcd on 8/11/20.
+//  Copyright © 2020 Trungld. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "PROFILE"
+    }
+
+
+
+    @IBAction func logoutPressed(_ sender: Any) {
+    }
+    
+}
