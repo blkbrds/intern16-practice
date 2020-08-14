fdbd076d4d381cdc98af486774d4d2ee36ce06fc
Nguyen Tran T <nguyen.tran@asiantech.vn>
2020-08-14 17:53:42 +0700

    hoàn thành graph 1

--- 06_Drawing/NguyenTran/Baitap6/Baitap6/Baitap1/Baitap1ViewController.swift
+++ 06_Drawing/NguyenTran/Baitap6/Baitap6/Baitap1/Baitap1ViewController.swift
@@ -7,44 +7,86 @@
 //
 
 import UIKit
+import Foundation
+
+struct YValueData {
+    var startValue: CGFloat
+    var gap: CGFloat
+}
 
 class Baitap1ViewController: UIViewController {
-    
+
     var barChart = GraphicsDraw()
+    let dateData = ["今日", "2021/01/01"]
+    let yValueData = YValueData(startValue: 500, gap: 50)
+    let pointData: Array<CGFloat> = [500, 510, 530, 557, 568, 590, 600]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         createXYasis()
-        createValueLabel(startValue: 999, gap: 1)
+        createValueLabel(startValue: yValueData.startValue, gap: yValueData.gap)
+        createDateLabel(dateValue: dateData)
+        createChart(data: pointData)
+        createPoint(data: pointData)
         view.addSubview(barChart)
     }
-    
+
     private func createXYasis() {
         barChart.backgroundColor = #colorLiteral(red: 0.8629845977, green: 0.9938684106, blue: 0.8738953471, alpha: 1)
-        let frame = CGRect(x: 20, y: UIScreen.main.bounds.height / 3, width: UIScreen.main.bounds.width - 40 , height: UIScreen.main.bounds.height / 3)
+        let frame = CGRect(x: 20, y: 200, width: UIScreen.main.bounds.width - 40, height: 350)
         barChart.frame = frame
-        barChart.drawLine(start: CGPoint(x: 35, y: 30), end: CGPoint(x: 35, y: frame.size.height - 45))
-        barChart.drawLine(start: CGPoint(x: 35, y: frame.size.height - 45), end: CGPoint(x: frame.size.width - 20, y: frame.size.height - 45))
+        barChart.drawLine(start: CGPoint(x: 40, y: 30), end: CGPoint(x: 40, y: 310))
+        barChart.drawLine(start: CGPoint(x: 40, y: 310), end: CGPoint(x: frame.size.width - 20, y: 310))
+        barChart.drawLine(start: CGPoint(x: frame.size.width - 25, y: 300), end: CGPoint(x: frame.size.width - 25, y: 310))
     }
-    
-    private func createValueLabel(startValue: Int, gap: Int) {
-        let xStart: Int = 0
-        var yStart: Int = 200
-        var valueLabel: Int = startValue
+
+    private func createValueLabel(startValue: CGFloat, gap: CGFloat) {
+        let xStart: CGFloat = 5
+        var yStart: CGFloat = 275
+        var valueLabel: CGFloat = startValue
         for _ in 1...3 {
-            barChart.addLabel(position: CGPoint(x: xStart, y: yStart), text: String(valueLabel))
+            barChart.addLabel(position: CGPoint(x: xStart, y: yStart - 15), text: String(Int(valueLabel)), fontSize: 14)
             valueLabel += gap
-            yStart -= ((Int(barChart.frame.size.height) - 85) / 3)
+            yStart -= 120
         }
     }
-    
 
+    private func createDateLabel(dateValue: [String]) {
+        barChart.addLabel(position: CGPoint(x: 30, y: barChart.frame.size.height - 35), text: dateValue[0], fontSize: 15)
+        barChart.addLabel(position: CGPoint(x: barChart.frame.size.width - 80, y: barChart.frame.size.height - 35), text: dateValue[1], fontSize: 15)
+    }
 
-}
+    private func configPointData(data: [CGFloat]) -> [CGPoint] {
+        let fullGap: CGFloat = yValueData.gap * 2
+        let barWidth = barChart.frame.size.width - 75
+        var result = [CGPoint]()
+        var yValue: CGFloat = 0
+        var xValue: CGFloat = 0
+        var tempY: CGFloat = 0
+        var tempX: CGFloat = 0
+        for i in 0...data.count - 1 {
+            tempY = (data[i] - yValueData.startValue) / fullGap
+            yValue = 275 - tempY * 240
+            tempX = CGFloat(Float(i) / Float((data.count - 1)))
+            xValue = tempX * (barWidth) + 40
+            result.append(CGPoint(x: xValue, y: yValue))
+        }
+        return result
+    }
 
-extension String {
-    func contentWidth(font: UIFont) -> CGFloat {
-        let size = (self as NSString).size(withAttributes: [.font: font])
-        return size.width + 30 - 6.67
+    private func createChart(data: [CGFloat]) {
+        var finalData = configPointData(data: data)
+        finalData.append(CGPoint(x: barChart.frame.size.width - 30, y: barChart.frame.size.height - 40))
+        finalData.append(CGPoint(x: 40, y: 310))
+        barChart.drawChart(pointValue: finalData)
+    }
+
+    private func createPoint(data: [CGFloat]) {
+        let finalData = configPointData(data: data)
+        print(finalData)
+        for i in 0...finalData.count - 1 {
+            barChart.addCirclePoint(position: CGPoint(x: finalData[i].x - 8, y: finalData[i].y - 8))
+        }
     }
 }
+
--- 06_Drawing/NguyenTran/Baitap6/Baitap6/Baitap1/CustomBarChart.swift
+++ 06_Drawing/NguyenTran/Baitap6/Baitap6/Baitap1/CustomBarChart.swift
@@ -7,68 +7,78 @@
 //
 
 import UIKit
-import Foundation
-
-enum ButtonType {
-    case value
-    case date
-}
 
 class GraphicsDraw: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     func drawLine(start: CGPoint, end: CGPoint) {
-        let line =  UIBezierPath()
+        let line = UIBezierPath()
         line.move(to: start)
         line.addLine(to: end)
         line.close()
-        
+
         let layer = CAShapeLayer()
         layer.strokeColor = UIColor.darkGray.cgColor
         layer.lineWidth = 1.0
         layer.path = line.cgPath
+
+        self.layer.addSublayer(layer)
+    }
+
+    func addLabel(position: CGPoint, text: String, fontSize: Float) {
+        let label = UILabel(frame: CGRect(x: position.x, y: position.y, width: 30, height: 30))
+        label.text = text
+        label.textAlignment = .left
+        label.textColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        guard let font = UIFont(name: "Arial", size: CGFloat(fontSize)) else { return }
+        label.font = label.font.withSize(CGFloat(fontSize))
+        label.frame.size.width = String(text).textWidth(font: font)
+        label.clipsToBounds = true
+        label.backgroundColor = .clear
+        self.addSubview(label)
+    }
+    
+    func drawChart(pointValue: [CGPoint]) {
+        let path = UIBezierPath()
+        for (i, p) in pointValue.enumerated() {
+            if i == 0 {
+                path.move(to: p)
+            } else {
+                path.addLine(to: p)
+            }
+        }
+        path.addLine(to: pointValue[0])
+        path.close()
+        
+        let layer = CAShapeLayer()
+        layer.fillColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+        layer.lineWidth = 0
+        layer.strokeColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+        layer.path = path.cgPath
         
         self.layer.addSublayer(layer)
     }
     
-//    func addLabel(type: buttonType, number: Int, text: String, fontSize: CGFloat? = nil) {
-//        let label = UILabel()
-//        label.frame.size = CGSize(width: 30, height: 40)
-//        switch type {
-//        case .value:
-//            label.center.x = CGFloat(35 + (number - 1) * (self.frame.width - 50))
-//            label.center.y = 45 / 2
-//        case .date:
-//            label.center.x = 35 / 2
-//            label.center.y =
-//
-//        }
-//        let label = UILabel(frame: CGRect(x: position.x, y: position.y, width: 30, height: 50))
-//
-//        label.text = text
-//        label.textAlignment = .center
-//        label.textColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
-//    }
-    
-    func addLabel(position: CGPoint, text: String, fontSize: CGFloat? = nil) {
-        let label = UILabel(frame: CGRect(x: position.x, y: position.y, width: 30, height: 50))
-        label.text = text
-        label.textAlignment = .left
-        label.textColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
-        if let size = fontSize {
-            label.font = UIFont.systemFont(ofSize: size)
-        } else {
-            label.adjustsFontSizeToFitWidth = true
-        }
+    func addCirclePoint(position: CGPoint) {
+        let label = UILabel(frame: CGRect(x: position.x, y: position.y, width: 16, height: 16))
+        label.layer.cornerRadius = 8
+        label.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        label.clipsToBounds = true
         self.addSubview(label)
     }
-    
 
+}
+
+extension String {
+    func textWidth(font: UIFont) -> CGFloat {
+        let size = (self as NSString).size(withAttributes: [.font: font])
+        return size.width + 5
+    }
 }
