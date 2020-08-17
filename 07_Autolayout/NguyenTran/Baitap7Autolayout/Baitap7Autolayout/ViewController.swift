753914ec5d94b2d11e1a42d4e8fd59468d8f3eac
Nguyen Tran T <nguyen.tran@asiantech.vn>
2020-08-17 09:32:41 +0700

    Bai tap 1, 2

---
+++ 07_Autolayout/NguyenTran/Baitap7Autolayout/Baitap7Autolayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Baitap7Autolayout
+//
+//  Created by MBA0245P on 8/16/20.
+//  Copyright © 2020 Tran Thao Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
