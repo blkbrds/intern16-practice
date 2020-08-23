c2974c7240fcda2d097166aba991d857f5d6d928
Nguyen Tran T <nguyen.tran@asiantech.vn>
2020-08-23 21:18:00 +0700

    Xong bai tap 6

---
+++ 07_Autolayout/NguyenTran/Baitap7Autolayout/Baitap7Autolayout/Baitap6/Baitap6_2ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Baitap6_2ViewController.swift
+//  Baitap7Autolayout
+//
+//  Created by MBA0245P on 8/23/20.
+//  Copyright © 2020 Tran Thao Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class Baitap6_2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
