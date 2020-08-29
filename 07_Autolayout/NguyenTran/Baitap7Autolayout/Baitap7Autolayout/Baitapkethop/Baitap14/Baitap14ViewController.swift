f98dccefd11bd98cf1cc6bb1a60bca8baef398ce
Nguyen Tran T <nguyen.tran@asiantech.vn>
2020-08-29 19:55:03 +0700

    Xong bai tap 14

---
+++ 07_Autolayout/NguyenTran/Baitap7Autolayout/Baitap7Autolayout/Baitapkethop/Baitap14/Baitap14ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  Baitap14ViewController.swift
+//  Baitap7Autolayout
+//
+//  Created by MBA0245P on 8/28/20.
+//  Copyright © 2020 Tran Thao Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class Baitap14ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
