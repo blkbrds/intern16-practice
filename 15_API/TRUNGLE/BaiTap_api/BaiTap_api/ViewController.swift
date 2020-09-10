0603edc8b32610bdff7172449e1696b13be9e4a9
at-trungle <trung.le@asiantech.vn>
2020-09-10 09:31:16 +0700

    bai1

---
+++ 15_API/TRUNGLE/BaiTap_api/BaiTap_api/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaiTap_api
+//
+//  Created by Abcd on 8/18/20.
+//  Copyright © 2020 Trungld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
