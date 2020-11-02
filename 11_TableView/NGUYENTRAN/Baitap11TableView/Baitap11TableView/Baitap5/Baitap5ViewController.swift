09692ef75d5e75eb751ef2e3ad19101a561dd1b0
Nguyen Tran T <nguyen.tran@asiantech.vn>
2020-11-02 15:34:25 +0700

    Xong bai tap 5

---
+++ 11_TableView/NGUYENTRAN/Baitap11TableView/Baitap11TableView/Baitap5/Baitap5ViewController.swift
@@ -0,0 +1,95 @@
+//
+//  Baitap5ViewController.swift
+//  Baitap11TableView
+//
+//  Created by MBA0245P on 10/27/20.
+//  Copyright © 2020 Tran Thao Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class Baitap5ViewController: UIViewController {
+    
+    var contactsData: [String] = []
+    var contacts: [String] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "HOME"
+        loadData()
+        configTableView()
+        configSearchBar()
+    }
+    func loadData() {
+        guard let path = Bundle.main.url(forResource: "NameList", withExtension: ".plist") else { return }
+        guard let contactList = NSArray(contentsOf: path) as? [String]
+            else { return }
+        contactsData = contactList
+        contacts = contactsData
+    }
+
+    func configTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        tableView.dataSource = self
+    }
+    
+    func configSearchBar() {
+        searchBar.delegate = self
+    }
+    
+    func search(keyword: String) {
+        contacts = getContacts(keyword: keyword)
+        tableView.reloadData()
+    }
+    
+    func getContacts(keyword: String) -> [String] {
+        if keyword.trimmingCharacters(in: CharacterSet(charactersIn: " ")) == "" {
+            return contactsData
+        } else {
+            var data: [String] = []
+            for contact in contactsData {
+                if let _ = contact.range(of: keyword) {
+                    data.append(contact)
+                }
+            }
+            return data
+        }
+    }
+}
+
+extension Baitap5ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        cell.textLabel?.text = "\(contacts[indexPath.row])"
+        return cell
+    }
+}
+
+extension Baitap5ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        var currentText = ""
+        if let searchBarText = searchBar.text {
+            currentText = searchBarText
+        }
+        let keyword = (currentText as NSString).replacingCharacters(in: range, with: text)
+        search(keyword: keyword)
+        return true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text?.removeAll()
+        search(keyword: "")
+    }
+}
+
